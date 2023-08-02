a54591a6f80bddcafd68632d6c14705657aa03c1
sawirricardo <sawir.ricardo@gmail.com>
2023-08-02 21:25:54 +0700

    refactor category to have recursive relationship and sortable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
-class Category extends Model
+class Category extends Model implements Sortable
 {
     use HasFactory;
+    use SortableTrait;
+    use HasRecursiveRelationships;
 
     protected $fillable = [
         'name',
