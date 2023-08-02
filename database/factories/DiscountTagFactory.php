daab5c680003a665e7d11228238172014ea62f4c
sawirricardo <sawir.ricardo@gmail.com>
2023-08-02 23:05:34 +0700

    remove auditable columns

--- database/factories/DiscountTagFactory.php
+++ database/factories/DiscountTagFactory.php
@@ -19,9 +19,6 @@ class DiscountTagFactory extends Factory
         return [
             'tag_id' => $this->faker->numberBetween(1, 10),
             'discount_id' => $this->faker->numberBetween(1, 10),
-            'created_by' => $this->faker->numberBetween(1, 20),
-            'updated_by' => $this->faker->numberBetween(1, 20),
-            'deleted_by' => null,
         ];
     }
 }
