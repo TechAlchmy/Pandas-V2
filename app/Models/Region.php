3fe2583b3c0f295fa7f88c5b39f384433b2f23b7
sawirricardo <sawir.ricardo@gmail.com>
2023-08-03 17:11:36 +0700

    make region auditable

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Concerns\InteractsWithAuditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
     use HasFactory;
+    use InteractsWithAuditable;
 
     public static function boot()
     {
