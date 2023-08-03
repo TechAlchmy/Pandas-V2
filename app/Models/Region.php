9707ef59aa763f48ed9f04525242fa07db2e3474
sawirricardo <sawir.ricardo@gmail.com>
2023-08-03 17:52:10 +0700

    bugfix region auditable

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -25,19 +25,4 @@ class Region extends Model
     {
         return $this->hasMany(BrandRegion::class);
     }
-
-    public function createdBy()
-    {
-        return $this->belongsTo(User::class, 'created_by');
-    }
-
-    public function updatedBy()
-    {
-        return $this->belongsTo(User::class, 'updated_by');
-    }
-
-    public function deletedBy()
-    {
-        return $this->belongsTo(User::class, 'deleted_by');
-    }
 }
