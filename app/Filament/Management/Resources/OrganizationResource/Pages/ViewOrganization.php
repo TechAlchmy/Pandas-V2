4b402bd87ee63a0b8ca8e58fbea6cedd5007545a
sawirricardo <sawir.ricardo@gmail.com>
2023-08-07 22:22:54 +0700

    use viewOrganization page as tenantProfile

--- app/Filament/Management/Resources/OrganizationResource/Pages/ViewOrganization.php
+++ app/Filament/Management/Resources/OrganizationResource/Pages/ViewOrganization.php
@@ -5,10 +5,31 @@ namespace App\Filament\Management\Resources\OrganizationResource\Pages;
 use App\Filament\Management\Resources\OrganizationResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class ViewOrganization extends ViewRecord
 {
     protected static string $resource = OrganizationResource::class;
+
+    public static function getLabel(): string
+    {
+        return 'My Company';
+    }
+
+    public static function getSlug(): string
+    {
+        return 'profile';
+    }
+
+    public static function canView(Model $tenant): bool
+    {
+        return true;
+    }
+
+    public function mount(int | string | null $record = null): void
+    {
+        $this->record = filament()->getTenant();
+    }
 
     protected function getHeaderActions(): array
     {
--- app/Providers/Filament/ManagementPanelProvider.php
+++ app/Providers/Filament/ManagementPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Management\Resources\OrganizationResource\Pages\ViewOrganization;
 use App\Models\Organization;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -28,6 +29,7 @@ class ManagementPanelProvider extends PanelProvider
             ->path('management')
             ->darkMode(false)
             ->tenant(Organization::class)
+            ->tenantProfile(ViewOrganization::class)
             ->colors([
                 'primary' => Color::Amber,
             ])
