5d5a5b602fba3d3a94557ff9b7713ba02a88e2c0
sawirricardo <sawir.ricardo@gmail.com>
2023-08-09 21:24:24 +0700

    add invite

--- app/Filament/Resources/OrganizationResource/RelationManagers/UsersRelationManager.php
+++ app/Filament/Resources/OrganizationResource/RelationManagers/UsersRelationManager.php
@@ -3,13 +3,18 @@
 namespace App\Filament\Resources\OrganizationResource\RelationManagers;
 
 use App\Filament\Resources\UserResource;
+use App\Models\OrganizationInvitation;
+use App\Models\User;
+use App\Notifications\OrganizationInvitationCreatedNotification;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Notification;
 
 class UsersRelationManager extends RelationManager
 {
@@ -57,6 +62,101 @@ class UsersRelationManager extends RelationManager
             ->headerActions([
                 Tables\Actions\CreateAction::make()
                     ->url(fn ($livewire) => UserResource::getUrl('create', ['organization_id' => $livewire->ownerRecord->getKey()])),
+                Tables\Actions\Action::make('invite')
+                    ->model(OrganizationInvitation::class)
+                    ->form([
+                        Forms\Components\Grid::make()
+                            ->schema([
+                                Forms\Components\TextInput::make('email')
+                                    ->required()
+                                    ->email(),
+                                Forms\Components\Toggle::make('is_manager')
+                                    ->required()
+                                    ->default(false),
+                            ]),
+                    ])
+                    ->mutateFormDataUsing(function ($data, $livewire) {
+                        $data['organization_id'] = $livewire->ownerRecord->getKey();
+                        return $data;
+                    })
+                    ->before(function ($action, $data) {
+                        $user = User::query()
+                            ->withTrashed()
+                            ->firstWhere('email', $data['email']);
+
+                        if (empty($user)) {
+                            $record = OrganizationInvitation::query()
+                                ->where('organization_id', $data['organization_id'])
+                                ->where('email', $data['email'])
+                                ->first();
+
+                            if (! $record) {
+                                return;
+                            }
+
+                            Notifications\Notification::make()
+                                ->warning()
+                                ->title('This email has been invited already')
+                                ->actions([
+                                    Notifications\Actions\Action::make('resend')
+                                        ->button()
+                                        ->action(function () use ($record) {
+                                            Notification::sendNow(
+                                                $record,
+                                                new OrganizationInvitationCreatedNotification($record)
+                                            );
+                                        }),
+                                ])
+                                ->persistent()
+                                ->send();
+
+                            $action->halt();
+                        }
+
+                        if ($user->trashed()) {
+                            Notifications\Notification::make()
+                                ->warning()
+                                ->title('This email is currently suspended!')
+                                ->persistent()
+                                ->actions([
+                                    Notifications\Actions\Action::make('view')
+                                        ->button()
+                                        ->url(UserResource::getUrl('edit', ['record' => $user]))
+                                        ->openUrlInNewTab(),
+                                ])
+                                ->send();
+
+                            $action->halt();
+                        }
+
+                        if ($user->organization_id) {
+                            Notifications\Notification::make()
+                                ->warning()
+                                ->title('This email is currently part of an organization!')
+                                ->persistent()
+                                ->actions([
+                                    Notifications\Actions\Action::make('view')
+                                        ->button()
+                                        ->url(UserResource::getUrl('edit', ['record' => $user]))
+                                        ->openUrlInNewTab(),
+                                ])
+                                ->send();
+
+                            $action->halt();
+                        }
+                    })
+                    ->action(function ($data, $action) {
+                        $record = OrganizationInvitation::query()
+                            ->create($data);
+
+                        Notification::sendNow(
+                            $record,
+                            new OrganizationInvitationCreatedNotification($record)
+                        );
+
+                        $action->success();
+                    })
+                    ->successNotificationTitle('User has been invited!'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
