26c7858e64bb5a9ec4dca7627a90fc1a26af3c29
Daniel Kiss <dkiss002>
2023-08-09 20:23:22 +0100

    Panda Blade Welcome - Deals on Daily

--- resources/views/panda.blade.php
+++ resources/views/panda.blade.php
@@ -12,6 +12,54 @@
                 <x-btn-white class="">Learn More</x-btn-white>
             </div>
         </div>
+        <x-break :height="'200px'"/>
+
+        <div class="flex mx-auto px-5">
+            <div class="flex-1 p-4 flex flex-col justify-between">
+                <h1 class="font-editorial text-[50px] leading-[60px]">Health & Wellness</h1>
+                <div>
+                    <p class="uppercase font-aeonik text-[23px]">Up to 20% off</p>
+                    <p class="font-aeonik text-[20px] leading-[28px]">24-hour access to primary care for less</p>
+                </div>
+            </div>
+
+            <div class="flex-1 p-4 flex flex-col justify-between">
+                <h1 class="font-editorial text-[50px] leading-[60px]">Groceries</h1>
+                <div>
+                    <p class="uppercase font-aeonik text-[23px]">15% off</p>
+                    <p class="font-aeonik text-[20px] leading-[28px]">Fill your fridge with storewide discounts</p>
+                </div>
+            </div>
+
+            <div class="flex-1 p-4 flex flex-col justify-between">
+                <h1 class="font-editorial text-[50px] leading-[60px]">Apparel</h1>
+                <div>
+                    <p class="uppercase font-aeonik text-[23px]">Up to 20% off</p>
+                    <p class="font-aeonik text-[20px] leading-[28px]">Brands you know, scrubs, and workwear</p>
+                </div>
+            </div>
+
+            <div class="flex-1 p-4 flex flex-col justify-between">
+                <h1 class="font-editorial text-[50px] leading-[60px]">Entertainment</h1>
+                <div>
+                    <p class="uppercase font-aeonik text-[23px]">Dining, Daytrips, + More</p>
+                    <p class="font-aeonik text-[20px] leading-[28px]">Enjoy your favorite restaurants, movie theaters, and live events</p>
+                </div>
+            </div>
+
+            <div class="flex-1 p-4 flex flex-col justify-between">
+                <h1 class="font-editorial text-[50px] leading-[60px]">Travel</h1>
+                <div>
+                    <p class="uppercase font-aeonik text-[23px]">Jetset for Less</p>
+                    <p class="font-aeonik text-[20px] leading-[28px]">Exclusive packages to top destinations</p>
+                </div>
+            </div>
+        </div>
+        <x-break :height="'20px'"/>
+        <x-black-line/>
+    </section>
+    
+    {{-- //banner --}}
 
     </section>
     <x-footer/>
