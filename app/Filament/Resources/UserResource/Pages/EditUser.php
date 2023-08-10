d3d15497d47f63e46477642f9ecca0b3b05c389f
sawirricardo <sawir.ricardo@gmail.com>
2023-08-10 17:06:57 +0700

    Alert if user is suspended
    if the account is suspended do massage in the user profile page.

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -4,11 +4,33 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    public function mount(int|string $record): void
+    {
+        parent::mount($record);
+
+        if ($this->getRecord()->trashed()) {
+            Notification::make()
+                ->title('This user is suspended!')
+                ->color('danger')
+                ->persistent()
+                ->danger()
+                ->send();
+        }
+    }
+
+    public function getSubheading(): ?string
+    {
+        return $this->getRecord()->trashed()
+            ? 'Suspended'
+            : null;
+    }
 
     protected function getHeaderActions(): array
     {
