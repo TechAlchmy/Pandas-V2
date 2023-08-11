6a830cb147f0f342df370fb83759dbe3ceba3b67
sawirricardo <sawir.ricardo@gmail.com>
2023-08-11 16:33:24 +0700

    Create logo.blade.php

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center">
+    @svg('logo', 'w-16 md:w-24 h-auto')
+    <span class="hidden sm:inline-block ml-2 sm:text-2xl md:text-3xl">
+        panda people
+    </span>
+</div>
