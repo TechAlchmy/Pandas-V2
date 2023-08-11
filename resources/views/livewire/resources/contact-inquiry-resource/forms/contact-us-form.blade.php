72885b523ee47bcb06426aad6e0bd598b1ab7f6b
sawirricardo <sawir.ricardo@gmail.com>
2023-08-11 22:43:29 +0700

    add submit form

--- resources/views/livewire/resources/contact-inquiry-resource/forms/contact-us-form.blade.php
+++ resources/views/livewire/resources/contact-inquiry-resource/forms/contact-us-form.blade.php
@@ -1,3 +1,9 @@
 <div>
-    {{ $this->form }}
+    <form wire:submit="create">
+        {{ $this->form }}
+
+        <button type="submit" class="px-8 py-3 leading-6 text-gray-900 rounded-[70%] border border-transparent hover:border-black">
+            Submit
+        </button>
+    </form>
 </div>
