b1dbf59b64441d33dfdf5db8a1bb5345a90e8402
sawirricardo <sawir.ricardo@gmail.com>
2023-08-11 16:06:23 +0700

    clean up tech debt

--- app/Forms/Components/AuditableView.php
+++ app/Forms/Components/AuditableView.php
@@ -17,32 +17,40 @@ class AuditableView extends Tabs
                 Tabs\Tab::make('Edited By')
                     ->columns(3)
                     ->schema([
-                        Placeholder::make('edited_by')
+                        Placeholder::make('updatedBy.name')
+                            ->label('Edited By')
                             ->content(fn ($record) => $record->updatedBy?->name),
-                        Placeholder::make('Email')
+                        Placeholder::make('updatedBy.email')
+                            ->label('Email')
                             ->content(fn ($record) => $record->updatedBy?->email),
-                        Placeholder::make('Last updated')
-                            ->content(fn ($record) => $record->updated_at->format('m/d/Y h:i:s A')),
+                        Placeholder::make('updated_at')
+                            ->label('Last updated at')
+                            ->content(fn ($record) => $record->created_at?->format('m/d/Y h:i:s A')),
                     ]),
                 Tabs\Tab::make('Created By')
                     ->columns(3)
                     ->schema([
-                        Placeholder::make('created_by')
+                        Placeholder::make('createdBy.name')
+                            ->label('Created by')
                             ->content(fn ($record) => $record->createdBy?->name),
-                        Placeholder::make('email')
+                        Placeholder::make('createdBy.email')
+                            ->label('Email')
                             ->content(fn ($record) => $record->createdBy?->email),
                         Placeholder::make('created_at')
-                            ->content(fn ($record) => $record->created_at->format('m/d/Y h:i:s A')),
+                            ->label('Created at')
+                            ->content(fn ($record) => $record->updated_at?->format('m/d/Y h:i:s A')),
                     ]),
                 Tabs\Tab::make('Suspended By')
                     ->columns(3)
                     ->schema([
-                        Placeholder::make('deleted_by')
-                            ->label('Suspended By')
+                        Placeholder::make('deletedBy.name')
+                            ->label('Suspended by')
                             ->content(fn ($record) => $record->deletedBy?->name),
-                        Placeholder::make('email')
+                        Placeholder::make('deletedBy.email')
+                            ->label('Email')
                             ->content(fn ($record) => $record->deletedBy?->email),
-                        Placeholder::make('Suspended at')
+                        Placeholder::make('deleted_at')
+                            ->label('Suspended at')
                             ->content(fn ($record) => $record->deleted_at?->format('m/d/Y h:i:s A')),
                     ]),
             ]);
