77294cde02e948788c1a3d280cacc7b3a9f74bed
sawirricardo <sawir.ricardo@gmail.com>
2023-08-11 18:46:53 +0700

    use svg logo

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,6 +1,9 @@
+@props(['withText' => false])
 <div class="flex items-center">
-    @svg('logo', 'w-16 md:w-24 h-auto')
-    <span class="hidden sm:inline-block ml-2 sm:text-2xl md:text-3xl">
-        panda people
-    </span>
+    @svg('logo', 'h-8 md:h-10 w-auto')
+    @if ($withText)
+        <span class="hidden sm:inline-block ml-2 sm:text-xl md:text-2xl">
+            panda people
+        </span>
+    @endif
 </div>
