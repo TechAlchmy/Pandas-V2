c3925785dcdee449ad018dd3118d799ad0f443e3
sawirricardo <sawir.ricardo@gmail.com>
2023-08-11 22:41:32 +0700

    add contact inquiry model

---
+++ database/migrations/2023_08_11_153450_create_contact_inquiries_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('contact_inquiries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->longText('content');
+            $table->foreignIdFor(\App\Models\User::class, 'created_by_id')->nullable();
+            $table->foreignIdFor(\App\Models\User::class, 'updated_by_id')->nullable();
+            $table->foreignIdFor(\App\Models\User::class, 'deleted_by_id')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_inquiries');
+    }
+};
