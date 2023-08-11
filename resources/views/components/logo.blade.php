abe67eb8697ad47494f841f0e22cbbac40b211fd
sawirricardo <sawir.ricardo@gmail.com>
2023-08-11 20:55:49 +0700

    use $attributes->twMerge

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,6 +1,6 @@
 @props(['withText' => false])
 <div class="flex items-center">
-    @svg('logo', 'h-8 md:h-10 w-auto')
+    @svg('logo', '', $attributes->twMerge(['h-8 md:h-10 w-auto text-black'])->getAttributes())
     @if ($withText)
         <span class="hidden sm:inline-block ml-2 sm:text-xl md:text-2xl">
             panda people
