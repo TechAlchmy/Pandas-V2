0deaf031e0e8e1eefbb69fc8800edcd74eb84b33
sawirricardo <sawir.ricardo@gmail.com>
2023-08-14 21:55:51 +0700

    streamline with signin input

--- resources/views/components/global-search.blade.php
+++ resources/views/components/global-search.blade.php
@@ -7,7 +7,7 @@
             <div class="flex shadow-sm transition duration-75 bg-white border border-black">
                 <div class="min-w-0 flex-1">
                     <form action="search">
-                        <x-input placeholder="Search Deals" type="search" name="q" />
+                        <x-input class="p-2" placeholder="Search Deals" type="search" name="q" />
                     </form>
                 </div>
                 <div class="flex items-center gap-x-3 ps-3 pe-2">
