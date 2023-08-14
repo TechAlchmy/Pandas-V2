b362db7563936910939837a274ae055022d06729
sawirricardo <sawir.ricardo@gmail.com>
2023-08-14 22:57:56 +0700

    Create textarea.blade.php

---
+++ resources/views/forms/components/textarea.blade.php
@@ -0,0 +1,16 @@
+<x-dynamic-component
+    :component="$getFieldWrapperView()"
+    :field="$field"
+>
+    <div class="border-b-[1.5px] py-2 border-black items-center font-medium" x-data="{ state: $wire.entangle('{{ $getStatePath() }}') }">
+        <span class="uppercase select-none caret-transparent flex">{{ $getPlaceholder() }} <span>{{ $isRequired() ? '*' : '' }}</span></span>
+        <textarea 
+            wire:model="{{ $getStatePath() }}"
+            {{ 
+                $attributes
+                    ->class(['block w-full border-none bg-white px-3 py-1.5 text-base text-gray-950 outline-none transition duration-75 placeholder:text-gray-400 disabled:bg-gray-50 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:bg-white/5 dark:text-white dark:placeholder:text-gray-500 dark:disabled:bg-transparent dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6'])
+            }}
+        >
+        </textarea>
+    </div>
+</x-dynamic-component>
