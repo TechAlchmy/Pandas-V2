c04d059e00c742350ff29351a208f211476aca77
sawirricardo <sawir.ricardo@gmail.com>
2023-08-14 22:38:58 +0700

    no need to limit width

--- resources/views/components/topbar/index.blade.php
+++ resources/views/components/topbar/index.blade.php
@@ -1,5 +1,5 @@
 <header class="bg-white border-b" x-data="{ mobileMenuOpened: false }">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <nav class="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
             <a href="/" class="-m-1.5 p-1.5">
                 <span class="sr-only">Panda People</span>
