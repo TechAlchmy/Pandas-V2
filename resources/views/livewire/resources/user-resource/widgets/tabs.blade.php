cc29fef355ac818eaf481cd6e3e15955595a39a8
sawirricardo <sawir.ricardo@gmail.com>
2023-08-14 16:43:26 +0700

    reorder menu

--- resources/views/livewire/resources/user-resource/widgets/tabs.blade.php
+++ resources/views/livewire/resources/user-resource/widgets/tabs.blade.php
@@ -7,7 +7,7 @@ state(['selected' => 0]);
 
 <div class="grid grid-cols-4 p-8">
     <ul class="divide-y text-xl">
-        @foreach (['My Details', 'My Preferences', 'My Orders', 'My Benefits', 'Daily Deals'] as $menu)
+        @foreach (['Daily Deals', 'My Benefits', 'My Details', 'My Preferences', 'My Orders'] as $menu)
             <li class="p-4" wire:key="{{ $loop->index }}">
                 <button x-on:click="$wire.set('selected', {{ $loop->index }})">{{ $menu }}</button>
             </li>
