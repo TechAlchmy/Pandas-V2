6f3efe99ad39d2edcdea8715a59de2ce70eed14b
sawirricardo <sawir.ricardo@gmail.com>
2023-08-14 22:03:10 +0700

    move button to the end

--- resources/views/livewire/resources/user-resource/forms/edit-profile-form.blade.php
+++ resources/views/livewire/resources/user-resource/forms/edit-profile-form.blade.php
@@ -1,8 +1,10 @@
 <div class="p-8">
     <form wire:submit.prevent="save">
         {{ $this->form }}
-        <x-button outlined class="inline-block mt-8">
-            Update
-        </x-button>
+        <div class="flex justify-end">
+            <x-button outlined class="inline-block mt-8">
+                Update
+            </x-button>
+        </div>
     </form>
 </div>
