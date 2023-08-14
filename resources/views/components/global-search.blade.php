c4fd2000d5fcd9fdd6be3e6cfcb9b77b08f6aed2
sawirricardo <sawir.ricardo@gmail.com>
2023-08-14 20:46:49 +0700

    streamline global-search design

--- resources/views/components/global-search.blade.php
+++ resources/views/components/global-search.blade.php
@@ -1,16 +1,14 @@
-<div class="fi-global-search flex items-center">
+<div class="flex items-center">
     <div class="relative">
         <div x-id="['input']" class="fi-global-search-field">
             <label x-bind:for="$id('input')" class="sr-only" for="input-8">
                 Global search
             </label>
-            <div
-                class="flex shadow-sm ring-1 transition duration-75 bg-white focus-within:ring-2 dark:bg-white/5 ring-gray-950/10 focus-within:ring-primary-600 dark:ring-white/20 dark:focus-within:ring-primary-500">
+            <div class="flex shadow-sm transition duration-75 bg-white border-b border-black">
                 <div class="min-w-0 flex-1">
-                    <input
-                        class="fi-input block w-full border-none bg-transparent p-1.5 text-base text-gray-950 outline-none transition duration-75 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6"
-                        autocomplete="off" placeholder="Search Deals" type="search" x-bind:id="$id('input')" wire:key="global-search.field.input" x-data="{}"
-                        id="input-8" data-has-alpine-state="true">
+                    <form action="search">
+                        <x-input placeholder="Search Deals" type="search" name="q" />
+                    </form>
                 </div>
                 <div class="flex items-center gap-x-3 ps-3 pe-2">
                     @svg('magnifying-glass', 'w-6 h-6')
