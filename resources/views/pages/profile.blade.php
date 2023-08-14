2a66660f8127c5095238c3cac1793ead08a84333
sawirricardo <sawir.ricardo@gmail.com>
2023-08-14 16:43:55 +0700

    refactor into a livewire component
    this will require the upsell banner to query data

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,16 +1,6 @@
 <x-layouts.app>
     <x-topbar />
-    <div class="bg-black text-center text-white p-8 lg:flex lg:justify-between lg:items-center">
-        <h2 class="text-6xl font-light">
-            Now Offering Housing
-        </h2>
-        <div class="py-4 lg:py-0"></div>
-        <div>
-            <x-link href="/help" class="px-8 py-3 text-base rounded-[70%] border border-white">
-                Learn More
-            </x-link>
-        </div>
-    </div>
+    <livewire:resources.user-resource.widgets.upsell-widget />
     <div class="flex p-8 justify-between">
         <h1 class="text-4xl lg:text-7xl">My Panda</h1>
         <p class="hidden lg:block">{{ auth()->user()->organization?->name }}</p>
