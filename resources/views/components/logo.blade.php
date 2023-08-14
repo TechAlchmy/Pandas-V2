81e410655651566f69823267cf8a45a55ec3b8f7
sawirricardo <sawir.ricardo@gmail.com>
2023-08-14 22:23:47 +0700

    update margin

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -2,7 +2,7 @@
 <div class="flex items-center">
     @svg('logo', '', $attributes->twMerge(['h-8 md:h-10 w-auto text-black'])->getAttributes())
     @if ($withText)
-        <span class="hidden sm:inline-block ml-2 sm:text-xl md:text-2xl">
+        <span class="hidden sm:inline-block ml-6 sm:text-xl md:text-2xl">
             panda people
         </span>
     @endif
