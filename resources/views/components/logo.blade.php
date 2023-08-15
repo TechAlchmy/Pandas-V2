bab43e5c444b29591dd2e658c9a641ee4e680635
sawirricardo <sawir.ricardo@gmail.com>
2023-08-15 20:29:26 +0700

    modify logo

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,8 +1,8 @@
 @props(['withText' => false])
-<div class="flex items-center">
-    @svg('logo', '', $attributes->twMerge(['h-8 md:h-10 w-auto text-black'])->getAttributes())
+<div class="flex items-center gap-4">
+    @svg('logo', '', $attributes->twMerge(['h-8 md:h-12 w-auto text-black'])->getAttributes())
     @if ($withText)
-        <span class="hidden sm:inline-block ml-6 sm:text-xl md:text-2xl">
+        <span class="hidden sm:inline-block ml-6 sm:text-xl md:text-2xl lg:text-3xl">
             panda people
         </span>
     @endif
