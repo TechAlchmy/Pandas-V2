1065e4c5bc97d1157592f13624050f4ea0a7f071
sawirricardo <sawir.ricardo@gmail.com>
2023-08-15 20:35:11 +0700

    Create [id].blade.php

---
+++ resources/views/pages/deals/[id].blade.php
@@ -0,0 +1,21 @@
+@php
+    $record = \App\Models\Discount::query()
+        ->where('slug', $id)
+        ->firstOrFail();
+@endphp
+<x-layouts.app>
+    <div class="grid grid-cols-1 lg:grid-cols-2 px-[min(6.99vw,50px)] py-8 gap-6">
+        <div class="bg-gray"></div>
+        <div class="">
+            <h1 class="text-4xl">
+                {{ $record->name }}
+            </h1>
+            <div>{!! $record->description !!}</div>
+            <div>
+                <x-button outlined size="lg">
+                    Redeem
+                </x-button>
+            </div>
+        </div>
+    </div>
+</x-layouts.app>
