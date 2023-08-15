d048b1dd42f2b10a9246105e2a0f1a932acfe4a3
sawirricardo <sawir.ricardo@gmail.com>
2023-08-15 20:34:55 +0700

    Update text-input.blade.php

--- resources/views/forms/components/text-input.blade.php
+++ resources/views/forms/components/text-input.blade.php
@@ -1,9 +1,21 @@
-<x-dynamic-component
-    :component="$getFieldWrapperView()"
-    :field="$field"
->
+<x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
     <div class="border-b-[1.5px] py-2 border-black flex gap-x-1 items-center font-medium" x-data="{ state: $wire.entangle('{{ $getStatePath() }}') }">
-        <span class="uppercase select-none caret-transparent flex">{{ $getPlaceholder() }} <span>{{ $isRequired() ? '*' : '' }}</span></span>
-        <x-input x-model="state" :disabled="$isDisabled()" />
+        <div class="flex">
+            <span class="uppercase select-none caret-transparent">
+                {{ $getPlaceholder() }}
+            </span>
+            <span>{{ $isRequired() ? '*' : '' }}</span>
+        </div>
+
+        <x-input
+            {{ $attributes->merge([
+                'autocapitalize' => $getAutocapitalize(),
+                'autocomplete' => $getAutocomplete(),
+                'autofocus' => $isAutofocused(),
+                'disabled' => $isDisabled(),
+                'required' => $isRequired(),
+                'type' => $getType() ?? 'text',
+            ]) }}
+            x-model="state" />
     </div>
 </x-dynamic-component>
