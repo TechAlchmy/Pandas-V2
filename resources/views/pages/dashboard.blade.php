e698cb2d7738f6ae676f327875d042cb9d28709a
sawirricardo <sawir.ricardo@gmail.com>
2023-08-15 20:33:57 +0700

    refactor to use new layout, add middleware

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,10 +1,13 @@
+<?php
+use function Laravel\Folio\{middleware};
+
+middleware(['auth', 'verified']);
+?>
 <x-layouts.app>
-    <x-topbar />
     <livewire:resources.user-resource.widgets.upsell-widget />
     <div class="flex p-8 justify-between">
         <h1 class="text-4xl lg:text-7xl">My Panda</h1>
         <p class="hidden lg:block">{{ auth()->user()->organization?->name }}</p>
     </div>
     <livewire:resources.user-resource.widgets.profile-tabs />
-    <livewire:footer />
 </x-layouts.app>
