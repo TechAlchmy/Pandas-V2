87016507d3f26bb99d4882d8ea9e9b1fbdd9fbc6
sawirricardo <sawir.ricardo@gmail.com>
2023-08-15 20:30:59 +0700

    Update guest-content.blade.php

--- resources/views/components/guest-content.blade.php
+++ resources/views/components/guest-content.blade.php
@@ -1,23 +1,21 @@
-
-<div class="flex flex-col lg:flex-row mx-auto w-full xl:px-10" 
-    style="max-width: 1920px;">
+<div class="flex flex-col lg:flex-row mx-auto w-full xl:px-10 max-w-[1920px]">
     <div class="w-full">
         <div class=" flex justify-start  px-[min(8%,50px)]">
-            <h1 id="dynamicColorText" class="font-editorial text-[min(80px,9.2vw)] lg:text-[100px] xl:text-[120px] mt-12 mr-10 absolute leading-[85px] lg:leading-[130px] p-1">Explore a<br class="max-xl:hidden"> new <br class="hidden max-xl:block">  world<br class="max-xl:hidden"> of benefits...</h1>
+            <h1 id="dynamicColorText" class="font-editorial text-[min(80px,9.2vw)] lg:text-[100px] xl:text-[120px] mt-12 mr-10 absolute leading-[85px] lg:leading-[130px] p-1">Explore a<br class="max-xl:hidden"> new <br
+                    class="hidden max-xl:block"> world<br class="max-xl:hidden"> of benefits...</h1>
         </div>
         <div id="imageContainer" class="flex max-h-[660px] justify-center">
-            <img src="{{ asset('storage/banners/banner-guest.png') }}" alt="Collages images" class="max-w-[1500px] h-auto "/>
+            <img src="{{ asset('storage/banners/banner-guest.png') }}" alt="Collages images" class="max-w-[1500px] h-auto " />
         </div>
     </div>
-    
+
 </div>
-<div class=" mx-auto w-full px-[min(5%,30px)] xl:px-0" 
-    style="max-width: 1920px;">
+<div class=" mx-auto w-full px-[min(5%,30px)] xl:px-0 max-w-[1920px]">
     <div class="mx-auto max-w-screen-2xl">
         <div class="flex flex-row-reverse content-conteiner">
             <div class="ellipse-container">
-                
-                <svg viewbox="0 0 200 200" class="svg-style " >
+
+                <svg viewbox="0 0 200 200" class="svg-style ">
                     <ellipse cx="95" cy="106" rx="90" ry="35" style="fill:none;stroke:black;stroke-width:0.4"></ellipse>
                 </svg>
                 <div class="">
@@ -28,25 +26,22 @@
             <div class="w-4/5 lg:w-3/5 flex flex-col md:flex-row  p-[50px] mt-[50px] text-container">
                 <div class=" flex flex-col md:px-10 ">
                     <p class="mb-5 font-aeonik text=[20px] uppercase">What is Panda People?</p>
-                    <p class= "font-aeonik">Real benefits for real people start with Panda. 
-                        We’re here to help you thrive in ways that matter 
-                        to you, so you can give more when it’s go time. Get 
-                        started on your journey to more life, more growth, 
+                    <p class="font-aeonik">Real benefits for real people start with Panda.
+                        We’re here to help you thrive in ways that matter
+                        to you, so you can give more when it’s go time. Get
+                        started on your journey to more life, more growth,
                         and more play.
                     </p>
                 </div>
-                <div class=" flex flex-col ">
-                    <p class="mb-5 max-md:mt-10  font-aeonik text=[20px] uppercase">OUR SITES</p>
-                    <div class="h-[120px] guest-buttons">
-                       <x-btn-black class="mb-5" :link="route('profile.edit')">For Employers</x-btn-black>
-                        <x-btn-black  :link="route('profile.edit')">For Employess</x-btn-black>
-                 
+                <div class="">
+                    <p class="mb-5 max-md:mt-10 font-aeonik text-2xl uppercase">OUR SITES</p>
+                    <div class="h-[120px] guest-buttons space-y-4">
+                        <x-link size="lg" href="/dashboard" class="bg-black text-white px-8">For Employers</x-link>
+                        <x-link size="lg" href="/dashboard" class="bg-black text-white px-8">For Employees</x-link>
                     </div>
-                    
                 </div>
             </div>
-            
+
         </div>
     </div>
 </div>
-
