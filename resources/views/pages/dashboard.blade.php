1793ae4e728e392080ff74976398a0c11ed8fe74
sawirricardo <sawir.ricardo@gmail.com>
2023-08-16 21:33:54 +0700

    add mx-auto and max-w-[1920px]

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -7,7 +7,7 @@ name('dashboard');
 <x-layouts.app>
     <livewire:resources.user-resource.widgets.upsell-widget />
 
-    <div class="flex px-[min(6.99vw,50px)] py-8 justify-between">
+    <div class="flex px-[min(6.99vw,50px)] max-w-[1920px] mx-auto py-8 justify-between">
         <h1 class="text-4xl lg:text-7xl">My Panda</h1>
         <p class="hidden lg:block">{{ auth()->user()->organization?->name }}</p>
     </div>
--- resources/views/pages/deals/index.blade.php
+++ resources/views/pages/deals/index.blade.php
@@ -4,7 +4,7 @@ use function Laravel\Folio\{name};
 name('deals.index');
 ?>
 <x-layouts.app>
-    <section class="px-[min(6.99vw,50px)] py-8 lg:flex">
+    <section class="px-[min(6.99vw,50px)] py-8 max-w-[1920px] mx-auto lg:flex">
         <h1 class="text-2xl lg:text-4xl">
             Live More with Panda-Powered Deals + Discounts
         </h1>
