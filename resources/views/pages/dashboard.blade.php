dcec1023aebeaa86f06be8f0c0a37c401a0cc1bd
sawirricardo <sawir.ricardo@gmail.com>
2023-08-16 15:55:25 +0700

    add vertical padding

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,8 +6,8 @@ name('dashboard');
 ?>
 <x-layouts.app>
     <livewire:resources.user-resource.widgets.upsell-widget />
-    
-    <div class="flex px-[min(6.99vw,50px)] justify-between">
+
+    <div class="flex px-[min(6.99vw,50px)] py-8 justify-between">
         <h1 class="text-4xl lg:text-7xl">My Panda</h1>
         <p class="hidden lg:block">{{ auth()->user()->organization?->name }}</p>
     </div>
