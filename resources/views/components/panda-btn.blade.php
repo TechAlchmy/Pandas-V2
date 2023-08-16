f848a9420dff0509ba749a5d3b7fed3bcda4007d
sawirricardo <sawir.ricardo@gmail.com>
2023-08-16 21:55:32 +0700

    remove unused files

--- resources/views/components/panda-btn.blade.php
+++
@@ -1,5 +0,0 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 
-'font-aeonik panda-submit inline-flex items-center px-4 py-2 bg-white  disabled:opacity-25 transition ease-in-out 
-duration-150']) }}>
-    {{ $slot }}
-</button>
--- resources/views/components/panda-form-submit.blade.php
+++
@@ -1,5 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 
-'font-aeonik panda-submit inline-flex items-center px-4 py-2 disabled:opacity-25 transition ease-in-out 
-duration-150']) }}>
-    {{ $slot }}
-</button>
