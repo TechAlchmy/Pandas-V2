6c4a0d523c38b8e543e7797da6d28a024156a68e
sawirricardo <sawir.ricardo@gmail.com>
2023-08-17 20:22:10 +0700

    add value if any filter.search

--- resources/views/components/global-search.blade.php
+++ resources/views/components/global-search.blade.php
@@ -7,7 +7,7 @@
             <div class="flex shadow-sm transition duration-75 bg-white border border-black">
                 <div class="min-w-0 flex-1">
                     <form action="/deals">
-                        <x-input class="p-2" placeholder="Search Deals" type="search" name="filter[search]" />
+                        <x-input class="p-2" placeholder="Search Deals" type="search" name="filter[search]" value="{{ request('filter.search') }}" />
                     </form>
                 </div>
                 <div class="flex items-center gap-x-3 ps-3 pe-2">
