a4b8ce7c4572e773a6375021e6a080dc285501f3
sawirricardo <sawir.ricardo@gmail.com>
2023-08-17 20:21:57 +0700

    fix styling in bigger screen

--- resources/views/components/list-recently-viewed.blade.php
+++ resources/views/components/list-recently-viewed.blade.php
@@ -3,7 +3,7 @@
 @endphp
 
 @if ($recentlyViewed->isNotEmpty())
-    <section class="px-[min(6.99vw,50px)] py-8 bg-neutral-200">
+    <section class="px-[min(6.99vw,50px)] py-8 bg-neutral-200 max-w-[1920px] mx-auto">
         <h3 class="text-4xl">Recently Viewed</h3>
         <x-hr />
         <div class="grid grid-cols-2 lg:grid-cols-4 gap-6">
