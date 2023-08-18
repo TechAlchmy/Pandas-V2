a085e2864e78384f6f845ebafb5eef3627e26ff2
sawirricardo <sawir.ricardo@gmail.com>
2023-08-18 20:00:30 +0700

    just use x-a so whenever user clicks on the shopping bag button, it redirects to checkout

--- resources/views/components/cart-button.blade.php
+++ resources/views/components/cart-button.blade.php
@@ -1,10 +1,7 @@
-<x-a href="/checkout" class="lg:hidden -m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-    @svg('shopping-bag', 'w-6')
-</x-a>
 <div class="relative" x-data="{ popoverOpen: false }">
-    <button x-on:cart-item-added.window="popoverOpen = true" class="hidden -m-2.5 lg:inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+    <x-a href="/checkout" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
         @svg('shopping-bag', 'w-6')
-    </button>
+    </x-a>
     <div x-ref="panel" x-cloak x-show="popoverOpen" x-on:click.away="popoverOpen = false" class="hidden lg:block absolute top-12 right-0 z-10 bg-white p-6 min-w-[24rem] border border-black">
         <div class="space-y-4">
             <div class="flex justify-between gap-6">
