f54a2f588fc827a363952d7b3c3708b876e70dbd
sawirricardo <sawir.ricardo@gmail.com>
2023-08-18 22:25:59 +0700

    bugfix shopping bag icon

--- resources/views/components/cart-button.blade.php
+++ resources/views/components/cart-button.blade.php
@@ -1,6 +1,6 @@
-<div class="relative" x-data="{ popoverOpen: false }" x-on:cart-item-added.window="popoverOpen = true">
+<div class="relative inline-flex" x-data="{ popoverOpen: false }" x-on:cart-item-added.window="popoverOpen = true">
     <x-a href="/checkout" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-        @svg('shopping-bag', 'w-6')
+        @svg('shopping-bag', 'w-6 h-6')
     </x-a>
     <div x-ref="panel" x-cloak x-transition x-show="popoverOpen" x-on:click.away="popoverOpen = false" class="hidden lg:block absolute top-12 right-0 z-10 bg-white p-6 min-w-[24rem] border border-black">
         <div class="space-y-4">
