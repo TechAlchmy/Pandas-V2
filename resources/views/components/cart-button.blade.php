de4d2e82d1ae2997f73e90271f4c921a7738a36f
sawirricardo <sawir.ricardo@gmail.com>
2023-08-18 20:03:55 +0700

    fix should open when cart item added and transition

--- resources/views/components/cart-button.blade.php
+++ resources/views/components/cart-button.blade.php
@@ -1,8 +1,8 @@
-<div class="relative" x-data="{ popoverOpen: false }">
+<div class="relative" x-data="{ popoverOpen: false }" x-on:cart-item-added.window="popoverOpen = true">
     <x-a href="/checkout" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
         @svg('shopping-bag', 'w-6')
     </x-a>
-    <div x-ref="panel" x-cloak x-show="popoverOpen" x-on:click.away="popoverOpen = false" class="hidden lg:block absolute top-12 right-0 z-10 bg-white p-6 min-w-[24rem] border border-black">
+    <div x-ref="panel" x-cloak x-transition x-show="popoverOpen" x-on:click.away="popoverOpen = false" class="hidden lg:block absolute top-12 right-0 z-10 bg-white p-6 min-w-[24rem] border border-black">
         <div class="space-y-4">
             <div class="flex justify-between gap-6">
                 <h3 class="text-xl font-light">New Item has been added</h3>
