65c4a564ce1e62be45de9a18860c47f09c494b49
sawirricardo <sawir.ricardo@gmail.com>
2023-08-24 21:38:29 +0700

    remove call to action enum

--- app/Enums/DiscountCallToActionEnum.php
+++ app/Enums/DiscountCallToActionEnum.php
@@ -6,10 +6,8 @@ use Filament\Support\Contracts\HasLabel;
 
 enum DiscountCallToActionEnum: int implements HasLabel
 {
-    case AddToCart = 0;
-    case RedeemNow = 1;
-    case GetCode = 2;
-    case GoToSite = 3;
+    case GetCode = 0;
+    case GoToSite = 1;
 
     public function getLabel(): ?string
     {
