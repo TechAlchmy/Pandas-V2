05feded6c40217aebbff924598aa062258844b6d
sawirricardo <sawir.ricardo@gmail.com>
2023-08-25 15:05:00 +0700

    Create OrderApprovedNotification.php

---
+++ app/Notifications/OrderApprovedNotification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OrderApprovedNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(
+        protected $order
+    ) {}
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('Thank you for your purchase!')
+                    ->line('Order ' . $this->order->order_column)
+                    ->action('View Orders', route('dashboard', ['activeTab' => 4]))
+                    ->line('Thank you for using panda!');
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
