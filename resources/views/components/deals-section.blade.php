a5951bd6ca09746a23befb5db9792abaca2f4b70
sawirricardo <sawir.ricardo@gmail.com>
2023-08-28 21:27:44 +0700

    add animation to deals-section

--- resources/views/components/deals-section.blade.php
+++ resources/views/components/deals-section.blade.php
@@ -1,10 +1,10 @@
 @props(['records', 'title' => null])
 
 @if ($records->isNotEmpty())
-    <section class="px-[min(6.99vw,50px)] py-8 bg-neutral-200 max-w-[1920px] mx-auto">
-        <h3 class="text-4xl">{{ $title }}</h3>
+    <section x-data="{ shown: false }" x-intersect.once="shown = true" class="px-[min(6.99vw,50px)] py-8 bg-neutral-200 max-w-[1920px] mx-auto">
+        <h3 x-show="shown" x-transition.opacity.duration.1000ms class="text-4xl">{{ $title }}</h3>
         <x-hr />
-        <div class="grid grid-cols-2 lg:grid-cols-4 gap-6">
+        <div x-show="shown" x-transition.opacity.duration.1000ms.delay.250ms class="grid grid-cols-2 lg:grid-cols-4 gap-6">
             @foreach ($records as $record)
                 <x-deal-card :record="$record" />
             @endforeach
