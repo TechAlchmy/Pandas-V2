761c205dcb39a6fe6541df863a43529b1dc02ce7
sawirricardo <sawir.ricardo@gmail.com>
2023-08-28 20:38:33 +0700

    animate footer

--- resources/views/components/footer/index.blade.php
+++ resources/views/components/footer/index.blade.php
@@ -1,11 +1,13 @@
-<footer class="w-full flex flex-col justify-between px-[min(6.99vw,50px)] lg:pl-12 h-[75px] lg:h-[100px] items-center mx-auto max-w-[1920px]">
-    <div class="h-48"></div>
-    <div class="w-full">
-        <x-hr />
-        <div class="w-full my-5">
-            <div class="flex justify-start mt-10">
-                <p class="text-[20px] mr-10">Panda People® {{ date('Y') }} © All Rights Reserved</p>
-                <a href="#" class="text-[20px] hover:text-blue-800 ml-10">Privacy Policy</a>
+<footer x-data="{ shown: false }" x-intersect.once="shown = true">
+    <div class="w-full flex flex-col justify-between px-[min(6.99vw,50px)] lg:pl-12 h-[75px] lg:h-[100px] items-center mx-auto max-w-[1920px]">
+        <div class="h-48"></div>
+        <div x-show="shown" x-transition:enter.duration.1000ms.opactiy class="w-full">
+            <x-hr />
+            <div class="w-full my-5">
+                <div class="flex justify-start mt-10">
+                    <p class="text-[20px] mr-10 animate-fadeIn">Panda People® {{ date('Y') }} © All Rights Reserved</p>
+                    <a href="#" class="text-[20px] hover:text-blue-800 ml-10">Privacy Policy</a>
+                </div>
             </div>
         </div>
     </div>
