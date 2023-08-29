ad5cac490c8d1ea110742e149b8dcfb9fb63d392
sawirricardo <sawir.ricardo@gmail.com>
2023-08-29 22:47:12 +0700

    add padding

--- resources/views/livewire/resources/organization-invitation-resource/pages/accept-invitation.blade.php
+++ resources/views/livewire/resources/organization-invitation-resource/pages/accept-invitation.blade.php
@@ -1,4 +1,4 @@
-<x-filament-panels::page.simple class="max-w-xl mx-auto">
+<x-filament-panels::page.simple class="max-w-xl mx-auto px-[min(6.99vw,50px)] py-8">
     <x-filament-panels::form wire:submit="register">
         {{ $this->form }}
 
