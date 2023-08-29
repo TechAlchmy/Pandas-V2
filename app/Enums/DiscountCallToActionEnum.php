19d8c1dfb5f175ebc4cb1080427adf121743cf24
sawirricardo <sawir.ricardo@gmail.com>
2023-08-29 20:39:04 +0700

    enable add to cart and redeem now action

--- app/Enums/DiscountCallToActionEnum.php
+++ app/Enums/DiscountCallToActionEnum.php
@@ -8,6 +8,8 @@ enum DiscountCallToActionEnum: int implements HasLabel
 {
     case GetCode = 0;
     case GoToSite = 1;
+    case AddToCart = 2;
+    case RedeemNow = 3;
 
     public function getLabel(): ?string
     {
