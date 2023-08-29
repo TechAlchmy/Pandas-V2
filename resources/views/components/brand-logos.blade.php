ddcae56360d16aee57cb08d79f2b5dcf8c853173
sawirricardo <sawir.ricardo@gmail.com>
2023-08-29 17:02:00 +0700

    add links to category when clicked

--- resources/views/components/brand-logos.blade.php
+++ resources/views/components/brand-logos.blade.php
@@ -4,30 +4,32 @@
     @foreach ($categories as $category)
         <div class="relative py-6 group">
             <h2 class="font-editorial text-5xl leading-[70px] text-center">{{ $category->name }}</h2>
-            <div class="invisible opacity-0 translate-y-5 group-hover:visible group-hover:opacity-100 group-hover:translate-y-0 duration-500 transition-transform ease-out absolute inset-y-0 bg-black w-full">
-                <div class="relative flex items-center overflow-x-hidden h-full">
-                    <div class="animate-marquee flex whitespace-nowrap items-center gap-4">
-                        @if (app()->isLocal())
-                            @foreach (['adidas-white', 'boss', 'nb', 'nike_white', 'puma', 'reebok', 'sketchers'] as $logo)
-                                <img class="max-w-[150px] max-h-24 invert" src="{{ asset('storage/logo/' . $logo . '.png') }}" alt="{{ $logo }} Logo">
+            <x-a :href="route('deals.index', ['filter' => ['category_id' => $category->getKey()]])">
+                <div class="invisible opacity-0 translate-y-5 group-hover:visible group-hover:opacity-100 group-hover:translate-y-0 duration-500 transition-transform ease-out absolute inset-y-0 bg-black w-full">
+                    <div class="relative flex items-center overflow-x-hidden h-full">
+                        <div class="animate-marquee flex whitespace-nowrap items-center gap-4">
+                            @if (app()->isLocal())
+                                @foreach (['adidas-white', 'boss', 'nb', 'nike_white', 'puma', 'reebok', 'sketchers'] as $logo)
+                                    <img class="max-w-[150px] max-h-24 invert" src="{{ asset('storage/logo/' . $logo . '.png') }}" alt="{{ $logo }} Logo">
+                                @endforeach
+                            @endif
+                            @foreach ($category->brands as $brand)
+                                <img class="max-w-[150px] max-h-24 invert" src="{{ $brand->getFirstMediaUrl('logo') }}" alt="{{ $brand->name }} Logo">
                             @endforeach
-                        @endif
-                        @foreach ($category->brands as $brand)
-                            <img class="max-w-[150px] max-h-24 invert" src="{{ $brand->getFirstMediaUrl('logo') }}" alt="{{ $brand->name }} Logo">
-                        @endforeach
-                    </div>
-                    <div class="h-full animate-marquee2 absolute top-0 flex whitespace-nowrap items-center gap-4 ml-4">
-                        @if (app()->isLocal())
-                            @foreach (['adidas-white', 'boss', 'nb', 'nike_white', 'puma', 'reebok', 'sketchers'] as $logo)
-                                <img class="max-w-[150px] max-h-24 invert" src="{{ asset('storage/logo/' . $logo . '.png') }}" alt="{{ $logo }} Logo">
+                        </div>
+                        <div class="h-full animate-marquee2 absolute top-0 flex whitespace-nowrap items-center gap-4 ml-4">
+                            @if (app()->isLocal())
+                                @foreach (['adidas-white', 'boss', 'nb', 'nike_white', 'puma', 'reebok', 'sketchers'] as $logo)
+                                    <img class="max-w-[150px] max-h-24 invert" src="{{ asset('storage/logo/' . $logo . '.png') }}" alt="{{ $logo }} Logo">
+                                @endforeach
+                            @endif
+                            @foreach ($category->brands as $brand)
+                                <img class="max-w-[150px] max-h-24 invert" src="{{ $brand->getFirstMediaUrl('logo') }}" alt="{{ $brand->name }} Logo">
                             @endforeach
-                        @endif
-                        @foreach ($category->brands as $brand)
-                            <img class="max-w-[150px] max-h-24 invert" src="{{ $brand->getFirstMediaUrl('logo') }}" alt="{{ $brand->name }} Logo">
-                        @endforeach
+                        </div>
                     </div>
                 </div>
-            </div>
+            </x-a>
         </div>
     @endforeach
 </div>
