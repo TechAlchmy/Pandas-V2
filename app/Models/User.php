bda72f7052fcf1dc35acfa4b2d017f825bb5bd46
sawirricardo <sawir.ricardo@gmail.com>
2023-08-30 15:28:58 +0700

    remove docblock comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,21 +29,11 @@ class User extends Authenticatable implements FilamentUser, HasTenants, HasDefau
     use SoftDeletes;
     use InteractsWithMedia;
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
