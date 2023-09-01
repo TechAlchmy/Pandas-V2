336c555c8980186ac85ad1d71e7ebf7e9c1f7651
sawirricardo <sawir.ricardo@gmail.com>
2023-09-01 19:11:19 +0700

    record search to gather insight in list deals page

--- app/Livewire/Resources/DealResource/Pages/ListDeals.php
+++ app/Livewire/Resources/DealResource/Pages/ListDeals.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use App\Models\BrandOrganization;
 use App\Models\Category;
 use App\Models\Discount;
+use App\Models\DiscountInsight;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms;
@@ -27,6 +28,7 @@ class ListDeals extends Component implements HasForms
     public function mount()
     {
         $this->form->fill(request('filter'));
+        $this->js('$wire.recordSearch()');
     }
 
     public function form(Form $form): Form
@@ -140,5 +142,33 @@ class ListDeals extends Component implements HasForms
     {
         return collect($this->filter)
             ->contains(fn ($filter) => ! empty($filter));
+    }
+
+    public function recordSearch()
+    {
+        if (empty($this->filter['search'])) {
+             return;
+        }
+
+        $insight = DiscountInsight::query()
+            ->create([
+                'term' => $this->filter['search'],
+                'category_id' => $this->filter['category_id'],
+                'brand_id' => $this->filter['brand_id'],
+                'user_id' => auth()->id(),
+                'page' => $this->getPage(),
+            ]);
+
+        if ($this->deals->isNotEmpty()) {
+            $insight->discountInsightModels()
+                ->createMany($this->deals->map(fn ($record) => [
+                    'discount_id' => $record->getKey(),
+                ])->all());
+        }
+    }
+
+    public function updatedPage($page)
+    {
+        $this->recordSearch();
     }
 }
