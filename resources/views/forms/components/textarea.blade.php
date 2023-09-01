1d02d6f2b51e0f52c6cd12dd71b299c843a52a60
sawirricardo <sawir.ricardo@gmail.com>
2023-09-01 22:14:54 +0700

    stylize textarea

--- resources/views/forms/components/textarea.blade.php
+++ resources/views/forms/components/textarea.blade.php
@@ -9,7 +9,7 @@
             rows="{{ $getRows() }}"
             {{
                 $attributes
-                    ->class(['block w-full border-none bg-white p-0 text-base text-gray-950 outline-none transition duration-75 placeholder:text-gray-400 disabled:bg-gray-50 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:bg-white/5 dark:text-white dark:placeholder:text-gray-500 dark:disabled:bg-transparent dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6 focus:ring-0'])
+                    ->class(['bg-transparent block w-full border-none p-0 text-base text-gray-950 outline-none transition duration-75 placeholder:text-gray-400 disabled:bg-gray-50 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:bg-transparent dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6 focus:ring-0'])
             }}
         >
         </textarea>
