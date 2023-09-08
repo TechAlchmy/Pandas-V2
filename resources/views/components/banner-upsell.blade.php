f9e2ebbc4c4f29b4706da44e5a467b8db7967632
sawirricardo <sawir.ricardo@gmail.com>
2023-09-08 11:49:17 +0700

    bugfix banner upsell

--- resources/views/components/banner-upsell.blade.php
+++ resources/views/components/banner-upsell.blade.php
@@ -2,7 +2,7 @@
 @if ($record)
     <div class="bg-black text-center text-white px-[min(6.99vw,50px)] py-8">
         <div class="max-w-[1920px] mx-auto">
-            <div class="lg:flex lg:justify-between lg:items-center gap-6">
+            <div class="flex flex-col lg:flex-row justify-between items-center gap-6">
                 <div class="flex flex-col md:flex-row items-center gap-6 mx-auto lg:mx-0">
                     @if ($record->brand?->hasMedia('logo'))
                         {{ $record->brand?->getFirstMedia('logo')->img()->attributes(['class' => 'invert']) }}
