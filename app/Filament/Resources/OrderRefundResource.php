c616ab515f127def12dd321737e5a57b26436e32
sawirricardo <sawir.ricardo@gmail.com>
2023-09-15 10:59:11 +0700

    disable bulk action

--- app/Filament/Resources/OrderRefundResource.php
+++ app/Filament/Resources/OrderRefundResource.php
@@ -69,9 +69,9 @@ class OrderRefundResource extends Resource
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ])
             ->emptyStateActions([
                 // Tables\Actions\CreateAction::make(),
