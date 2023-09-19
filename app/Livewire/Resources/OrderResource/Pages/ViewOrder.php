fdd35514d338f5d6282fcb79fb9391ed487eee16
sawirricardo <sawir.ricardo@gmail.com>
2023-09-19 23:36:36 +0700

    refactor

--- app/Livewire/Resources/OrderResource/Pages/ViewOrder.php
+++ app/Livewire/Resources/OrderResource/Pages/ViewOrder.php
@@ -15,6 +15,7 @@ use Filament\Infolists\Contracts\HasInfolists;
 use Filament\Infolists;
 use Filament\Infolists\Infolist;
 use Filament\Notifications\Notification;
+use Illuminate\Support\Carbon;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
@@ -46,8 +47,69 @@ class ViewOrder extends Component implements HasForms, HasInfolists
                     ->getStateUsing(fn ($record) => $record->order_total / 100)
                     ->money('USD'),
                 Infolists\Components\RepeatableEntry::make('orderDetails')
+                    ->label('Details')
                     ->columnSpanFull()
                     ->columns(['default' => 2, 'md' => 4, 'lg' => 5])
+                    ->hintAction(Infolists\Components\Actions\Action::make('refund')
+                        ->fillForm(fn ($record) => [
+                            'order_details' => $record->orderDetails,
+                        ])
+                        ->form(fn ($record) => [
+                            Forms\Components\Repeater::make('order_details')
+                                ->columns(3)
+                                ->reorderable(false)
+                                ->maxItems($record->orderDetails->count())
+                                ->minItems($record->orderDetails->count())
+                                ->itemLabel(fn ($state) => \implode(' - ', [
+                                    \data_get($state, 'discount.brand.name'),
+                                    \data_get($state, 'discount.name'),
+                                ]))
+                                ->schema([
+                                    Forms\Components\TextInput::make('order_detail_refund.quantity')
+                                        ->formatStateUsing(fn ($state, $get) => $state ?? $get('quantity'))
+                                        ->numeric()
+                                        ->required()
+                                        ->minValue(1)
+                                        ->maxValue(fn ($record, $get) => $record->orderDetails->firstWhere('id', $get('id'))?->quantity)
+                                        ->helperText(fn ($record, $get) => 'Max: '.$record->orderDetails->firstWhere('id', $get('id'))?->quantity)
+                                        ->suffix(function ($record, $get, $state) {
+                                            $orderDetail = $record->orderDetails->firstWhere('id', $get('id'));
+                                            $orderDetail->quantity = $state;
+                                            return format_money($orderDetail->total / 100, 'USD');
+                                        })
+                                        ->disabled(fn ($get) => ! empty($get('order_detail_refund.id'))),
+                                    Forms\Components\TextInput::make('order_detail_refund.note')
+                                        ->label('Reason')
+                                        ->disabled(fn ($get) => ! empty($get('order_detail_refund.id')))
+                                        ->maxLength(255),
+                                    Forms\Components\Placeholder::make('order_detail_refund.approved_at')
+                                        ->visible(fn ($get) => ! empty($get('order_detail_refund.id')))
+                                        ->content(fn ($state) => empty($state) ? 'In Review' : Carbon::parse($state)?->format('d M Y')),
+                                ]),
+                        ])
+                        ->action(function ($action, $data, $record) {
+                            foreach ($data['order_details'] as $detail) {
+                                $orderDetail = $record->orderDetails->firstWhere('id', $detail['id']);
+
+                                if (! $orderDetail) {
+                                    continue;
+                                }
+
+                                if ($orderDetail->orderDetailRefund?->approved_at) {
+                                    continue;
+                                }
+
+                                OrderDetailRefund::query()
+                                    ->updateOrCreate([
+                                        'order_detail_id' => $detail['id'],
+                                    ], [
+                                        'quantity' => \data_get($detail, 'order_detail_refund.quantity'),
+                                        'note' => \data_get($detail, 'order_detail_refund.note'),
+                                    ]);
+                            }
+
+                            $action->success();
+                        }))
                     ->schema([
                         Infolists\Components\TextEntry::make('discount.name')
                             ->getStateUsing(fn ($record) => implode(' - ', [
@@ -63,60 +125,60 @@ class ViewOrder extends Component implements HasForms, HasInfolists
                         Infolists\Components\TextEntry::make('total')
                             ->getStateUsing(fn ($record) => $record->total / 100)
                             ->money('USD'),
-                        Infolists\Components\Actions::make([
-                            Infolists\Components\Actions\Action::make('request_refund')
-                                ->link()
-                                ->hidden(fn ($record) => $record->orderDetailRefund)
-                                ->modalHeading(fn ($record) => \implode(' - ', [
-                                    'Request refund for ' . $record->discount->brand->name,
-                                    $record->discount->name,
-                                ]))
-                                ->modalSubmitActionLabel('Request Refund')
-                                ->fillForm(fn ($record) => [
-                                    'quantity' => $record->quantity,
-                                ])
-                                ->form(fn ($record) => [
-                                    Forms\Components\Grid::make()
-                                        ->schema([
-                                            Forms\Components\TextInput::make('quantity')
-                                                ->live()
-                                                ->numeric()
-                                                ->required()
-                                                ->helperText('Max: ' . $record->quantity)
-                                                ->maxValue($record->quantity)
-                                                ->minValue(1)
-                                                ->suffix(function ($state) use ($record) {
-                                                    $record->quantity = $state;
-                                                    return format_money($record->total / 100, 'USD');
-                                                }),
-                                            Forms\Components\TextInput::make('note')
-                                                ->label('Reason to refund')
-                                                ->maxLength(255),
-                                        ]),
-                                ])
-                                ->successNotificationTitle('Successfully Requested to refund the item')
-                                ->successNotification(function ($notification, $record) {
-                                    auth()->user()->notify(new SendUserOrderRefundInReview(
-                                        $record->order->order_column,
-                                        \implode(' - ', [
-                                            $record->discount->brand->name,
-                                            $record->discount->name,
-                                        ]),
-                                    ));
-                                    return $notification;
-                                })
-                                ->action(function ($action, $data, $record) {
-                                    OrderDetailRefund::query()
-                                        ->updateOrCreate([
-                                            'order_detail_id' => $record->getKey(),
-                                        ], [
-                                            'quantity' => $data['quantity'],
-                                            'note' => $data['note'],
-                                        ]);
+                        // Infolists\Components\Actions::make([
+                        //     Infolists\Components\Actions\Action::make('request_refund')
+                        //         ->link()
+                        //         ->hidden(fn ($record) => $record->orderDetailRefund)
+                        //         ->modalHeading(fn ($record) => \implode(' - ', [
+                        //             'Request refund for ' . $record->discount->brand->name,
+                        //             $record->discount->name,
+                        //         ]))
+                        //         ->modalSubmitActionLabel('Request Refund')
+                        //         ->fillForm(fn ($record) => [
+                        //             'quantity' => $record->quantity,
+                        //         ])
+                        //         ->form(fn ($record) => [
+                        //             Forms\Components\Grid::make()
+                        //                 ->schema([
+                        //                     Forms\Components\TextInput::make('quantity')
+                        //                         ->live()
+                        //                         ->numeric()
+                        //                         ->required()
+                        //                         ->helperText('Max: ' . $record->quantity)
+                        //                         ->maxValue($record->quantity)
+                        //                         ->minValue(1)
+                        //                         ->suffix(function ($state) use ($record) {
+                        //                             $record->quantity = $state;
+                        //                             return format_money($record->total / 100, 'USD');
+                        //                         }),
+                        //                     Forms\Components\TextInput::make('note')
+                        //                         ->label('Reason to refund')
+                        //                         ->maxLength(255),
+                        //                 ]),
+                        //         ])
+                        //         ->successNotificationTitle('Successfully Requested to refund the item')
+                        //         ->successNotification(function ($notification, $record) {
+                        //             auth()->user()->notify(new SendUserOrderRefundInReview(
+                        //                 $record->order->order_column,
+                        //                 \implode(' - ', [
+                        //                     $record->discount->brand->name,
+                        //                     $record->discount->name,
+                        //                 ]),
+                        //             ));
+                        //             return $notification;
+                        //         })
+                        //         ->action(function ($action, $data, $record) {
+                        //             OrderDetailRefund::query()
+                        //                 ->updateOrCreate([
+                        //                     'order_detail_id' => $record->getKey(),
+                        //                 ], [
+                        //                     'quantity' => $data['quantity'],
+                        //                     'note' => $data['note'],
+                        //                 ]);
 
-                                    $action->success();
-                                }),
-                        ]),
+                        //             $action->success();
+                        //         }),
+                        // ]),
                     ]),
             ]);
     }
@@ -130,7 +192,7 @@ class ViewOrder extends Component implements HasForms, HasInfolists
                 $query->with('orderDetailRefund', fn ($query) => $query->withTrashed())
                     ->with('discount.brand.media');
             })
-            // ->withExists(['orderRefund' => fn ($query) => $query->withTrashed()])
+            ->withExists(['orderDetailRefunds' => fn ($query) => $query->withTrashed()])
             ->firstWhere('uuid', $this->id);
     }
 }
