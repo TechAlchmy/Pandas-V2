5b287c28796d299105a62be385810e392daaf111
sawirricardo <sawir.ricardo@gmail.com>
2023-09-25 11:33:21 +0700

    add collect

--- app/Enums/DiscountVoucherTypeEnum.php
+++ app/Enums/DiscountVoucherTypeEnum.php
@@ -9,6 +9,11 @@ enum DiscountVoucherTypeEnum: int
     case AddToCart = 2;
     case RedeemNow = 3;
 
+    public static function collect()
+    {
+        return \collect(self::cases());
+    }
+
     public function getLabel(): ?string
     {
         return str($this->name)
