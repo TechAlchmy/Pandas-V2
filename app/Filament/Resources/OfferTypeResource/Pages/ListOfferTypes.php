d59cd599da560f709169c71cc0c33651afc2c52a
sawirricardo <sawir.ricardo@gmail.com>
2023-09-28 14:48:53 +0700

    bugfix create action

--- app/Filament/Resources/OfferTypeResource/Pages/ListOfferTypes.php
+++ app/Filament/Resources/OfferTypeResource/Pages/ListOfferTypes.php
@@ -13,7 +13,7 @@ class ListOfferTypes extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\CreateAction::make(),
+            Actions\CreateAction::make(),
         ];
     }
 }
