ff0e6684ad55816b6c7cedab5d9f6e260762c39e
sawirricardo <sawir.ricardo@gmail.com>
2023-10-19 13:26:57 +0700

    show different input value based on top up gift card or defined amounts

--- resources/views/livewire/resources/deal-resource/pages/view-deal.blade.php
+++ resources/views/livewire/resources/deal-resource/pages/view-deal.blade.php
@@ -34,7 +34,15 @@
                                         @endforeach
                                     </select>
                                 @endif
-                                <x-input class="lg:max-w-[50%] !border-solid border-black p-2" type="number" wire:model="quantity" min="1" />
+                                @if ($this->record->voucher_type == \App\Enums\DiscountVoucherTypeEnum::TopUpGiftCard)
+                                    <div class="flex items-center space-x-1 w-full">
+                                        <span>$</span>
+                                        <x-input class="w-full !border-solid border-black p-2" type="number" wire:model="amount" :min="$this->record->bh_min / 100" :max="$this->record->bh_max / 100" />
+                                    </div>
+                                @endif
+                                @if($this->record->voucher_type == \App\Enums\DiscountVoucherTypeEnum::DefinedAmountsGiftCard)
+                                    <x-input class="lg:max-w-[50%] !border-solid border-black p-2" type="number" wire:model="quantity" min="1" />
+                                @endif
                             </div>
                             <div class="flex gap-6 items-center">
                                 <x-button class="hover:bg-panda-green" x-on:click="$wire.addToCart();$wire.updateClicks()" outlined>
