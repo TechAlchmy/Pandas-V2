c510dc61206fb0e9f5eea56adb27d72126936cbf
Kiran Timsina <timsinakiran@gmail.com>
2023-10-20 16:23:46 +0545

    rework deals card

--- resources/views/components/deal-card.blade.php
+++ resources/views/components/deal-card.blade.php
@@ -1,19 +1,29 @@
 @props(['record'])
 
 <div class="space-y-2 mt-4 min-h-full flex flex-col">
-    <div class="min-h-[4rem]">
-        @if ($record->brand->hasMedia('logo'))
-        <x-a class="inline-block h-16 overflow-hidden" :href="route('deals.show', ['id' => $record->slug])">
-            {{ $record->brand->getFirstMedia('logo')->img()->attributes(['class' => 'object-contain max-w-[6rem] w-full h-full']) }}
-        </x-a>
-        @endif
+    <div class="flex-grow flex">
+        <div class="flex-1">
+            <x-a class="inline" :href="route('deals.show', ['id' => $record->slug])">
+                <h4 class="text-2xl font-light">{{ $record->brand->name }}</h4>
+            </x-a>
+            <p class="text-sm">{{ $record->name }}</p>
+        </div>
+
+        <div class="flex-shrink-0">
+            @if ($record->brand->hasMedia('logo'))
+            <x-a class="inline-block h-16 overflow-hidden" :href="route('deals.show', ['id' => $record->slug])">
+                {{ $record->brand->getFirstMedia('logo')->img()->attributes(['class' => 'object-contain max-w-[6rem] w-full h-full']) }}
+            </x-a>
+            @endif
+        </div>
     </div>
-    <x-a class="inline-block" :href="route('deals.show', ['id' => $record->slug])">
-        <h4 class="text-4xl font-light">{{ $record->brand->name }}</h4>
-    </x-a>
-    <p class="text-2xl">{{ $record->name }}</p>
-    <p>{{ $record->excerpt }}</p>
-    <!-- <div class="flex-grow"></div> -->
+
+
+
+    <div class="h-20">
+        <p class=" line-clamp-3">{{ $record->excerpt }}</p>
+    </div>
+    <div class="flex-grow"></div>
 
     <div>
         <x-link class="transition-transform duration-300 transform hover:scale-105 hover:bg-panda-green" outlined :href="route('deals.show', ['id' => $record->slug])">
