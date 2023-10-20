de6ca4a4c383e3af07634b4709828417392d1900
sawirricardo <sawir.ricardo@gmail.com>
2023-10-20 17:39:21 +0700

    bugfix wrong variable

--- app/Livewire/Resources/OrderResource/Pages/CreateOrder.php
+++ app/Livewire/Resources/OrderResource/Pages/CreateOrder.php
@@ -153,10 +153,10 @@ class CreateOrder extends Component implements HasForms, HasActions
                 }
             }
             if ($item['itemable']->voucher_type == DiscountVoucherTypeEnum::TopUpGiftCard) {
-                if ($this->record->bh_min >= $item['amount'] || $this->record->bh_max <= $item['amount']) {
+                if ($item['itemable']->bh_min >= $item['amount'] || $item['itemable']->bh_max <= $item['amount']) {
                     Notification::make()
                         ->danger()
-                        ->title('limit is ' . \Filament\Support\format_money($this->record->bh_min / 100, 'USD') . ' and ' . \Filament\Support\format_money($this->record->bh_max / 100, 'USD'))
+                        ->title('limit is ' . \Filament\Support\format_money($item['itemable']->bh_min / 100, 'USD') . ' and ' . \Filament\Support\format_money($item['itemable']->bh_max / 100, 'USD'))
                         ->send();
                     return;
                 }
