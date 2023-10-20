b7214c823dcb06b57add47be286699394af66243
Kiran Timsina <timsinakiran@gmail.com>
2023-10-20 09:20:24 +0545

    change button animation for see deals

--- resources/views/components/deal-card.blade.php
+++ resources/views/components/deal-card.blade.php
@@ -3,9 +3,9 @@
 <div class="space-y-2 mt-4 min-h-full flex flex-col">
     <div class="min-h-[4rem]">
         @if ($record->brand->hasMedia('logo'))
-            <x-a class="inline-block h-16 overflow-hidden" :href="route('deals.show', ['id' => $record->slug])">
-                {{ $record->brand->getFirstMedia('logo')->img()->attributes(['class' => 'object-contain max-w-[6rem] w-full h-full']) }}
-            </x-a>
+        <x-a class="inline-block h-16 overflow-hidden" :href="route('deals.show', ['id' => $record->slug])">
+            {{ $record->brand->getFirstMedia('logo')->img()->attributes(['class' => 'object-contain max-w-[6rem] w-full h-full']) }}
+        </x-a>
         @endif
     </div>
     <x-a class="inline-block" :href="route('deals.show', ['id' => $record->slug])">
@@ -14,9 +14,17 @@
     <p class="text-2xl">{{ $record->name }}</p>
     <p>{{ $record->excerpt }}</p>
     <!-- <div class="flex-grow"></div> -->
-    <div>
+
+    <!-- <div>
         <x-link class="hover:bg-panda-green" outlined :href="route('deals.show', ['id' => $record->slug])">
             See Deals
         </x-link>
+    </div> -->
+
+    <div class="inline-block">
+        <x-link class="bg-gradient-to-l from-panda-green to-panda-green bg-no-repeat bg-right transition-all duration-500 ease-in-out transform hover:scale-105 hover:bg-left hover:bg-panda-green" style="background-size: 200% 100%;" outlined :href="route('deals.show', ['id' => $record->slug])">
+            See Deals
+        </x-link>
     </div>
+
 </div>
