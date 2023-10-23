bc43004ebeee0e6474906bbba8885f6427091ffe
sawirricardo <sawir.ricardo@gmail.com>
2023-10-23 19:45:43 +0700

    fix whitespace in cart

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,8 +1,9 @@
-<x-layouts.base {{ $attributes }}>
+<x-layouts.base {{ $attributes }} class="flex flex-col min-h-screen">
     <x-topbar />
     @auth
         <x-topbar.extension />
     @endauth
     {{ $slot }}
+    <div class="flex-grow"></div>
     <x-footer.extra />
 </x-layouts.base>
