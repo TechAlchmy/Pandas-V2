0b55c0f39df2d33f218c1a4831521271a96d8aae
sawirricardo <sawir.ricardo@gmail.com>
2023-10-25 23:54:43 +0700

    add employer inquiry

---
+++ app/Livewire/Resources/EmployerInquiryResource/Forms/EmployerInquiryForm.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Livewire\Resources\EmployerInquiryResource\Forms;
+
+use App\Models\ContactInquiry;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+class EmployerInquiryForm extends Component implements HasForms
+{
+    use InteractsWithForms;
+
+    public $data;
+
+    public function mount()
+    {
+        $this->form->fill();
+    }
+
+    public function create()
+    {
+        $data = $this->form->getState();
+
+        $data['user_id'] = auth()->id();
+
+        ContactInquiry::query()->create($data);
+
+        Notification::make()
+            ->success()
+            ->title('Received! We will contact you back soon.')
+            ->send();
+
+        $this->form->fill();
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->statePath('data')
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->hiddenLabel()
+                    ->placeholder('Name')
+                    ->view('forms.components.text-input')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->hiddenLabel()
+                    ->placeholder('Email')
+                    ->view('forms.components.text-input')
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('content')
+                    ->required()
+                    ->hiddenLabel()
+                    ->placeholder('Message')
+                    ->view('forms.components.textarea')
+                    ->rows(6)
+                    ->maxLength(255),
+            ]);
+    }
+
+    #[Computed]
+    public function testimonials()
+    {
+        return [
+            ['Test 1', 'This is good!'],
+            ['Test 2', 'Panda has helped me!'],
+            ['Test 3', 'This is the benefits that I have wanted'],
+            ['Test 1', 'This is is awesome for you employees'],
+        ];
+    }
+}
---
+++ resources/views/livewire/resources/employer-inquiry-resource/forms/employer-inquiry-form.blade.php
@@ -0,0 +1,29 @@
+<section>
+    <div class="max-w-[1920px] mx-auto">
+        <div class="grid grid-cols-1 md:grid-cols-2">
+            <div>
+                <div class="relative min-h-[10rem] md:h-full w-full bg-cover" style="background-image: url({{ getMediaPath('assets/contact-us-banner.png') }})">
+                    <div class="absolute inset-0 p-6 space-y-4">
+                        <h3 class="text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-editorial" x-transition>The Benefits of tomorrow</h3>
+                        <p x-transition>
+                            Schedule a demo with an expert in Panda’s employee benefits packages and reward your workforce with perks that reduce their cost of living at less cost to you.
+
+                            The best part? These perks better retention and give you the edge in the competitive talent marketplace.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="p-8 space-y-4">
+                <h2 class="text-6xl font-light font-editorial">Schedule a demo with Panda</h2>
+                <form wire:submit.prevent="create">
+                    {{ $this->form }}
+                    <div class="flex justify-start">
+                        <x-button outlined class="inline-block mt-8">
+                            Send
+                        </x-button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
