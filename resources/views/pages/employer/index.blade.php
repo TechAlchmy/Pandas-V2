c336a7fe0f43fb4456ec20d807154777590ed9a3
sawirricardo <sawir.ricardo@gmail.com>
2023-10-27 15:39:59 +0700

    make title smaller

--- resources/views/pages/employer/index.blade.php
+++ resources/views/pages/employer/index.blade.php
@@ -39,7 +39,7 @@ name('employer');
         <div class="flex flex-col w-full mx-auto overflow-hidden lg:flex-row">
             <div class="space-y-24 px-[min(6.99vw,50px)] py-8 2xl:ml-60">
                 <div class="space-y-6">
-                    <h2 class="text-5xl font-editorial lg:text-7xl 2xl:text-9xl">What is Panda People</h2>
+                    <h2 class="text-5xl font-editorial lg:text-7xl 2xl:text-8xl">What is Panda People</h2>
                     <p>
                         Employee benefits aren’t black and white anymore. There’s a spectrum of perks that can make an
                         impact on
