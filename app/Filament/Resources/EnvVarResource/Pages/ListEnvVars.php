8d7f091bfd3a9c93791481e4e0d78ec40a4f8a12
sawirricardo <sawir.ricardo@gmail.com>
2023-10-31 16:59:45 +0700

    add ability to test mail

--- app/Filament/Resources/EnvVarResource/Pages/ListEnvVars.php
+++ app/Filament/Resources/EnvVarResource/Pages/ListEnvVars.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\EnvVarResource\Pages;
 
 use App\Filament\Resources\EnvVarResource;
+use App\Mail\TestMail;
+use Filament\Forms;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Mail;
 
 class ListEnvVars extends ListRecords
 {
@@ -14,6 +18,20 @@ class ListEnvVars extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Actions\Action::make('test_mail')
+                ->form([
+                    Forms\Components\TextInput::make('email')
+                        ->email()
+                        ->required(),
+                ])
+                ->action(function ($action, $data) {
+                    try {
+                        Mail::to($data['email'])->send(new TestMail);
+                    } catch (\Throwable $e) {
+                        logger()->error($e->getMessage());
+                    }
+                    $action->success();
+                }),
         ];
     }
 }
