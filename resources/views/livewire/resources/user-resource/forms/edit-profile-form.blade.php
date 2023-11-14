5576a91c659c3db3f45b916d39d424b46eec17ed
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-14 17:29:07 +0700

    add edit button

--- resources/views/livewire/resources/user-resource/forms/edit-profile-form.blade.php
+++ resources/views/livewire/resources/user-resource/forms/edit-profile-form.blade.php
@@ -2,6 +2,7 @@
     <div x-show="!$wire.isEditing" class="grid grid-cols-1 lg:grid-cols-4 gap-6 border-t pt-4">
         <div>
             <h2 class="text-xl font-bold">{{ auth()->user()->name }}</h2>
+            <button class="underline" x-on:click="$wire.$toggle('isEditing')">Edit</button>
         </div>
         <div class="space-y-2">
             <div>
