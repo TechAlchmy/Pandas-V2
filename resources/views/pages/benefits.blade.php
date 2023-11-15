e8b00356cad7f26b7ff4d135b6e643ae8e625626
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-15 09:56:45 +0700

    fix copywriting

--- resources/views/pages/benefits.blade.php
+++ resources/views/pages/benefits.blade.php
@@ -33,8 +33,8 @@ name('benefits');
                 <h1 x-show="shown" x-transition.duration.1500 class="max-w-xl text-3xl sm:text-6xl font-editorial">Get Ready to go beyond the basic</h1>
                 <div class="space-y-6 lg:ml-56 xl:ml-80">
                     <p class="">
-                        Real benefits for real people start with Panda. We’re here to help you thrive in ways that matters to you, so you can give more when it’s go time—in life and work.
-
+                        Real benefits for real people start with Panda. We’re here to help you thrive in ways that matters to you, so you can give more and get more when it’s go time—in life and work.
+                        
                         Explore the benefits your employer is providing through Panda People.
                     </p>
                     <x-link class="hover:bg-panda-green" outlined :href="route('dashboard', ['activeTab' => 1])">View My Benefits</x-link>
