52cc1ce439f250cb5e10d50d86f3cd136bebbcb9
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-15 09:50:47 +0700

    dont register orders in management portal nav

--- app/Filament/Management/Resources/OrderResource.php
+++ app/Filament/Management/Resources/OrderResource.php
@@ -20,6 +20,8 @@ class OrderResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
 
+    protected static bool $shouldRegisterNavigation = false;
+
     public static function form(Form $form): Form
     {
         return $form
