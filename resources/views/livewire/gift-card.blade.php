88a9a887d734ade46e75ba0331e90e27bd20e6ca
Kiran Timsina <timsinakiran@gmail.com>
2023-11-18 22:13:35 +0545

    set message for order processing and copy code

--- resources/views/livewire/gift-card.blade.php
+++ resources/views/livewire/gift-card.blade.php
@@ -101,10 +101,13 @@
                                                                                         <div class="min-w-0 flex-1">
                                                                                             <div class="">
                                                                                                 <div>
-                                                                                                    <div class="fi-in-text-item inline-flex items-center gap-1.5 text-sm leading-6 text-gray-950 dark:text-white  " style="">
+                                                                                                    <div class="cursor-pointer fi-in-text-item inline-flex items-center gap-1.5 text-sm leading-6 text-gray-950 dark:text-white">
 
-                                                                                                        <div class="">
-                                                                                                            {{ $gift['pin']}}
+                                                                                                        <div onclick="copyToClipboard('{{ $gift['pin']}}')" class="cursor-pointer flex items-center">
+                                                                                                            <span>{{ $gift['pin']}} </span>
+                                                                                                            <svg xmlns=" http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-2">
+                                                                                                                <path stroke-linecap=" round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+                                                                                                            </svg>
                                                                                                         </div>
 
                                                                                                     </div>
@@ -119,7 +122,7 @@
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            <div style="--col-span-default: 1 / -1;" class="col-[--col-span-default]">
+                                                            <div style=" --col-span-default: 1 / -1;" class="col-[--col-span-default]">
                                                                 <div class="fi-in-entry-wrp">
                                                                     <span class="text-sm font-medium leading-6 text-gray-950 dark:text-white">
                                                                         Card Number
@@ -137,8 +140,11 @@
                                                                                                 <div>
                                                                                                     <div class="fi-in-text-item inline-flex items-center gap-1.5 text-sm leading-6 text-gray-950 dark:text-white  " style="">
 
-                                                                                                        <div class="prose max-w-none dark:prose-invert [&amp;>*:first-child]:mt-0 [&amp;>*:last-child]:mb-0 prose-sm">
-                                                                                                            # {{ substr($gift['cardNumber'], 0, -1 * strlen($gift['pin']))}}
+                                                                                                        <div onclick="copyToClipboard('{{ substr($gift['cardNumber'], 0, -1 * strlen($gift['pin'])) }}')" class="cursor-pointer flex items-center">
+                                                                                                            <span># {{ substr($gift['cardNumber'], 0, -1 * strlen($gift['pin'])) }}</span>
+                                                                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-2">
+                                                                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />
+                                                                                                            </svg>
                                                                                                         </div>
 
                                                                                                     </div>
@@ -217,3 +223,14 @@
     </div>
     @endforeach
 </div>
+
+<script>
+    function copyToClipboard(text) {
+        navigator.clipboard.writeText(text).then(() => {
+            // Optional: Display a brief confirmation message
+            alert('Copied Sucessfully!');
+        }).catch(err => {
+            console.error('Error copying to clipboard: ', err);
+        });
+    }
+</script>
