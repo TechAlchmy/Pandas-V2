40136692a67b031628b875a577cf1df640c4a690
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-27 21:57:59 +0700

    style preference form

--- resources/views/livewire/resources/user-resource/forms/edit-preferences-form.blade.php
+++ resources/views/livewire/resources/user-resource/forms/edit-preferences-form.blade.php
@@ -1,8 +1,8 @@
 <div class="p-4 lg:p-8">
     <form wire:submit.prevent="save">
         {{ $this->form }}
-        <div class="flex justify-end">
-            <x-button type="submit" size="lg" outlined class="inline-block mt-8">
+        <div class="flex justify-start">
+            <x-button type="submit" size="lg" outlined class="inline-block mt-8 hover:bg-panda-green hover:border-transparent">
                 Update
             </x-button>
         </div>
