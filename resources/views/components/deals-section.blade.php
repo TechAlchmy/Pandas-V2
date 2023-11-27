1e4973bdbe94f60ece4a6ba5f0dba002372533db
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-27 22:56:32 +0700

    smaller title

--- resources/views/components/deals-section.blade.php
+++ resources/views/components/deals-section.blade.php
@@ -3,7 +3,7 @@
 @if ($records->isNotEmpty())
     <section x-data="{ shown: false }" x-intersect.once="shown = true" class="px-[min(6.99vw,50px)] py-8 bg-neutral-200">
         <div class="max-w-[1920px] mx-auto">
-            <h3 x-show="shown" x-transition.opacity.duration.1000ms class="text-4xl">{{ $title }}</h3>
+            <h3 x-show="shown" x-transition.opacity.duration.1000ms class="text-3xl">{{ $title }}</h3>
             <x-hr />
             <div x-show="shown" x-transition.opacity.duration.1000ms.delay.250ms class="grid grid-cols-2 lg:grid-cols-4 gap-6">
                 @foreach ($records as $record)
