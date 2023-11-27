6d79f0ad179c33c87aa29f2cd33cfc540aaf9114
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-27 22:56:47 +0700

    handle contact page to show testimonial

--- resources/views/livewire/resources/contact-inquiry-resource/forms/contact-us-form.blade.php
+++ resources/views/livewire/resources/contact-inquiry-resource/forms/contact-us-form.blade.php
@@ -1,7 +1,11 @@
+@props([
+    'testimonial' => false,
+])
 <section>
     <div class="max-w-[1920px] mx-auto">
         <div class="grid grid-cols-1 md:grid-cols-2">
             <div>
+                @if ($testimonial)
                 <div class="relative min-h-[10rem] md:h-full w-full bg-cover" style="background-image: url({{ getMediaPath('assets/contact-us-banner.png') }})">
                     <div x-data="@js(['index' => 0, 'testimonials' => $this->testimonials])" x-init="setInterval(() => {
                         if (index + 1 >= testimonials.length) {
@@ -9,14 +13,26 @@
                         } else {
                             index++;
                         }
-                    }, 5000)" class="absolute inset-0 p-6 space-y-4 text-white">
+                    }, 5000)" class="absolute inset-0 p-6 space-y-4">
                         <h3 class="text-4xl lg:text-6xl font-editorial" x-transition x-text="testimonials[index][1]"></h3>
                         <p x-text="testimonials[index][0]" x-transition>
                         </p>
                     </div>
                 </div>
+                @else
+                <div class="min-h-[10rem] md:h-full w-full bg-cover p-8 flex flex-col justify-between" style="background-image: url({{ getMediaPath('assets/contact-us-banner.png') }})">
+                    <h1 class="font-editorial text-6xl lg:text-7xl 2xl:text-8xl">Both of our ears are open.</h1>
+                    <p>
+                        Questions? Suggestions? They’re all welcome. 
+
+                        We’re here to make your Panda Portal as helpful as possible and are always looking for ways to make daily living easier. Like a panda leisurely seeking out its roots, shoots, and leaves for lunch.
+
+                        That said, we love prompt responses and won’t be leisurely about getting back to you.
+                    </p>
+                </div>
+                @endif
             </div>
-            <div class="p-8 space-y-4 bg-panda-green">
+            <div class="p-8 space-y-4 {{ $testimonial ? 'bg-panda-green' : '' }}">
                 <h2 class="text-6xl font-light font-editorial">Contact Us</h2>
                 <form wire:submit.prevent="create">
                     {{ $this->form }}
