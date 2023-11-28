1a8c743764af5a3c6b511c876f73c8ee3107aa8c
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-28 21:24:21 +0700

    make placeholder white

--- resources/views/livewire/resources/subscriber-resource/forms/create-subscriber-form.blade.php
+++ resources/views/livewire/resources/subscriber-resource/forms/create-subscriber-form.blade.php
@@ -17,7 +17,7 @@ $submit = function () {
 <form wire:submit.prevent="submit" class="text-white space-y-6">
     <h4 class="font-editorial">Panda People Newsletter</h4>
     <div class="py-2 border-b border-white flex gap-x-1 items-center max-w-xs">
-        <x-input type="email" class="text-lg" wire:model="email" placeholder="EMAIL" />
+        <x-input type="email" class="text-lg placeholder-white" wire:model="email" placeholder="EMAIL" />
     </div>
     <x-button type="submit" outlined color="white" class="inline-block">
         Sign Up
