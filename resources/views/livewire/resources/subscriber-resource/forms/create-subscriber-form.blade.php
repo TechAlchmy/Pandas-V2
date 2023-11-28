a8afb250b43a459f76278e093e732aed669d21cf
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-28 21:13:28 +0700

    use placeholder instead

--- resources/views/livewire/resources/subscriber-resource/forms/create-subscriber-form.blade.php
+++ resources/views/livewire/resources/subscriber-resource/forms/create-subscriber-form.blade.php
@@ -17,8 +17,7 @@ $submit = function () {
 <form wire:submit.prevent="submit" class="text-white space-y-6">
     <h4 class="font-editorial">Panda People Newsletter</h4>
     <div class="py-2 border-b border-white flex gap-x-1 items-center max-w-xs">
-        <span class="uppercase select-none caret-transparent">Email</span>
-        <x-input type="email" class="text-lg" wire:model="email" />
+        <x-input type="email" class="text-lg" wire:model="email" placeholder="EMAIL" />
     </div>
     <x-button type="submit" outlined color="white" class="inline-block">
         Sign Up
