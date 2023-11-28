da7f4665519648fec6a4544cea394fea0ba24a14
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-28 12:35:28 +0700

    smaller h1, smaller line length

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -7,7 +7,7 @@
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 <div></div>
                 <div class="p-8 text-white space-y-6 lg:space-y-8 z-[1]" x-data="{ shown: false }" x-intersect.once="shown = true">
-                    <h1 x-show="shown" x-transition.duration.2000 class="text-3xl lg:text-5xl font-editorial">
+                    <h1 x-show="shown" x-transition.duration.2000 class="text-3xl lg:text-4xl font-editorial max-w-sm">
                         Explore a new world of benefits that help <u>you</u> grow
                     </h1>
                     <div class="lg:ml-64 space-y-6 lg:space-y-8">
