bb9305d92637c80185ddfb46e6500554529fdd63
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-28 20:41:39 +0700

    bump login button

--- resources/views/livewire/resources/auth-resource/forms/login-form.blade.php
+++ resources/views/livewire/resources/auth-resource/forms/login-form.blade.php
@@ -1,7 +1,7 @@
 <div>
     <form wire:submit="authenticate">
         {{ $this->form }}
-        <x-button type="submit" size="lg" outlined class="inline-block mt-4 hover:bg-black hover:text-white">
+        <x-button type="submit" size="lg" outlined class="inline-block mt-8 hover:bg-black hover:text-white">
             Login
         </x-button>
     </form>
