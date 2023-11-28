574fd49f2ad640f0f7576706e77c5051bd2b6c16
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-28 20:52:32 +0700

    make $attributes

--- resources/views/components/deals-section.blade.php
+++ resources/views/components/deals-section.blade.php
@@ -1,7 +1,7 @@
 @props(['records', 'title' => null])
 
 @if ($records->isNotEmpty())
-    <section x-data="{ shown: false }" x-intersect.once="shown = true" class="px-[min(6.99vw,50px)] py-8 bg-neutral-200">
+    <section x-data="{ shown: false }" x-intersect.once="shown = true" {{ $attributes->class(['px-[min(6.99vw,50px)] py-8 bg-neutral-200']) }}>
         <div class="max-w-[1920px] mx-auto">
             <h3 x-show="shown" x-transition.opacity.duration.1000ms class="text-2xl font-editorial">{{ $title }}</h3>
             <x-hr />
