294a4d5c2e6ea63569b487bca344ce2ed557e9b9
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-30 22:28:43 +0700

    make z-index = 1

--- resources/views/livewire/resources/deal-resource/pages/list-deals.blade.php
+++ resources/views/livewire/resources/deal-resource/pages/list-deals.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <section class="px-[min(6.99vw,50px)] py-8 bg-neutral-200 lg:sticky lg:top-28 lg:z-10">
+    <section class="px-[min(6.99vw,50px)] py-8 bg-neutral-200 lg:sticky lg:top-28 lg:z-[1]">
         <div class="max-w-[1920px] mx-auto lg:flex lg:justify-between lg:gap-6">
             <h1 class="mb-8 text-2xl text-center lg:text-4xl lg:text-start lg:mb-0">
                 Live More with Panda-Powered Deals + Discounts
