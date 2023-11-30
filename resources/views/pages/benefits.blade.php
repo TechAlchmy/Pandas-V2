9f6a30915e2ca8319a2f877c727d3c14b9854ed5
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-11-30 23:01:23 +0700

    reemove

--- resources/views/pages/benefits.blade.php
+++ resources/views/pages/benefits.blade.php
@@ -85,11 +85,6 @@ name('benefits');
                             </li>
                         @endforeach
                     </ul>
-                    <div class="flex flex-col gap-6 lg:flex-row">
-                        <div class="lg:w-1/2">
-                            <x-link href="#" class="mx-auto hover:bg-panda-green hover:border-transparent" outlined>See Additional Benefits</x-link>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
