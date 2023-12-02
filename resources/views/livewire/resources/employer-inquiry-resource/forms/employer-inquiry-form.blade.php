c5926051e798aa264299e83e4a931878e87f0b75
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-12-02 11:24:56 +0700

    underline

--- resources/views/livewire/resources/employer-inquiry-resource/forms/employer-inquiry-form.blade.php
+++ resources/views/livewire/resources/employer-inquiry-resource/forms/employer-inquiry-form.blade.php
@@ -6,7 +6,7 @@
                     <div class="absolute inset-0 p-6 space-y-4">
                         <h3 class="text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-editorial" x-transition>The Benefits of tomorrow</h3>
                         <p x-transition>
-                            Schedule a demo with an expert in Panda’s employee benefits packages and reward your workforce with perks that reduce their cost of living at less cost to you.
+                            Schedule a demo with an expert in Panda’s employee benefits packages and reward your workforce with perks that reduce their cost of living at less cost to <u>you</u>.
 
                             The best part? These perks better retention and give you the edge in the competitive talent marketplace.
                         </p>
@@ -14,7 +14,7 @@
                 </div>
             </div>
             <div class="p-8 space-y-4">
-                <h2 class="text-6xl font-light font-editorial">Schedule a demo with Panda</h2>
+                <h2 class="text-6xl font-light font-editorial">Schedule a<br /> demo with Panda</h2>
                 <form wire:submit.prevent="create">
                     {{ $this->form }}
                     <div class="flex justify-start">
