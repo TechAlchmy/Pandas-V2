a0fc2b0b053e3a9627259c4a632e93dd484e1a5b
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-12-04 19:35:17 +0700

    smaller text

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -2,7 +2,7 @@
 <div class="flex items-center gap-4">
     @svg('logo', '', $attributes->twMerge(['h-7 md:h-10 w-auto text-black hover:animate-bounce'])->getAttributes())
     @if ($withText)
-        <span class="hidden sm:inline-block lg:ml-6 sm:text-xl md:text-2xl lg:text-3xl">
+        <span class="hidden sm:inline-block lg:ml-6 sm:text-xl xl:text-2xl">
             panda people
         </span>
     @endif
