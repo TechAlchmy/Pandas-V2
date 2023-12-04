91982fabfbc5a983e1a4fabddef1645541e2cb42
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-12-04 19:30:17 +0700

    smaller space

--- resources/views/livewire/resources/user-resource/widgets/list-daily-deals.blade.php
+++ resources/views/livewire/resources/user-resource/widgets/list-daily-deals.blade.php
@@ -9,21 +9,21 @@ $categories = computed(function () {
 });
 
 ?>
-<div class="grid grid-cols-1 lg:grid-cols-2 gap-6 bg-panda-green">
+<div class="grid grid-cols-1 gap-6 lg:grid-cols-2 bg-panda-green">
     <div class="grid grid-cols-1">
         <div class="place-self-center">
             <img class="max-h-[40rem]" src="{{ getMediaPath('assets/list-daily-deals.png') }}" />
         </div>
     </div>
     <div class="grid grid-cols-1">
-        <div class="space-y-4 p-4 lg:place-self-center">
+        <div class="p-4 space-y-4 lg:place-self-center">
             <h2 class="text-3xl font-editorial">
                 We’re here to help you afford more of what brings you joy and everyday essentials.
             </h2>
             <h5 class="text-xl">
                 Find everyday deals on:
             </h5>
-            <ul class="grid grid-cols-2">
+            <ul class="grid grid-cols-2 lg:max-w-sm">
                 @foreach ($this->categories as $category)
                     <li>
                         <x-a :href="route('deals.index', ['filter' => ['category_id' => $category->getKey()]])">{{ $category->name }}</x-a>
