fdf09d374a1437d44bce70df1e85b874ce5fab99
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-12-04 20:17:18 +0700

    fix style

--- resources/views/livewire/resources/user-resource/widgets/list-daily-deals.blade.php
+++ resources/views/livewire/resources/user-resource/widgets/list-daily-deals.blade.php
@@ -16,20 +16,22 @@ $categories = computed(function () {
         </div>
     </div>
     <div class="grid grid-cols-1">
-        <div class="p-4 space-y-4 lg:place-self-center">
-            <h2 class="text-3xl font-editorial">
+        <div class="p-4 space-y-12 lg:place-self-center">
+            <h2 class="text-3xl lg:text-4xl font-editorial">
                 We’re here to help you afford more of what brings you joy and everyday essentials.
             </h2>
-            <h5 class="text-xl">
-                Find everyday deals on:
-            </h5>
-            <ul class="grid grid-cols-2 lg:max-w-xs">
-                @foreach ($this->categories as $category)
-                    <li>
-                        <x-a :href="route('deals.index', ['filter' => ['category_id' => $category->getKey()]])">{{ $category->name }}</x-a>
-                    </li>
-                @endforeach
-            </ul>
+            <div class="space-y-6">
+                <h5 class="text-xl">
+                    Find everyday deals on:
+                </h5>
+                <ul class="grid grid-cols-2 lg:max-w-xs">
+                    @foreach ($this->categories as $category)
+                        <li>
+                            <x-a :href="route('deals.index', ['filter' => ['category_id' => $category->getKey()]])">{{ $category->name }}</x-a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
             <div class="flex justify-end md:block">
                 <x-link class="hover:bg-black hover:text-white" outlined :href="route('deals.index')">
                     Discover more deals
