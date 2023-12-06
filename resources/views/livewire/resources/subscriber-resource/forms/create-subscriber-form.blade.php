70739b0f8f2b4901faec39e614a7fdd8687ce069
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-12-06 08:22:37 +0700

    add validate, show error, reset email

--- resources/views/livewire/resources/subscriber-resource/forms/create-subscriber-form.blade.php
+++ resources/views/livewire/resources/subscriber-resource/forms/create-subscriber-form.blade.php
@@ -1,24 +1,35 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 
 state(['email']);
 
+rules(['email' => 'required|email']);
+
 $submit = function () {
+    $this->validate();
+
     \App\Models\Subscriber::query()->firstOrCreate(['email' => $this->email]);
 
     \Filament\Notifications\Notification::make()
         ->title('Subscribed!')
         ->success()
         ->send();
+
+    $this->reset('email');
 };
 ?>
 
-<form wire:submit.prevent="submit" class="text-white space-y-6">
+<form wire:submit.prevent="submit" class="space-y-6 text-white">
     <h4 class="font-editorial">Panda People Newsletter</h4>
-    <div class="py-2 border-b border-white flex gap-x-1 items-center max-w-xs">
+    <div class="flex items-center max-w-xs py-2 border-b border-white gap-x-1">
         <x-input type="email" class="text-lg placeholder-white" wire:model="email" placeholder="EMAIL" />
     </div>
+    @error('email')
+        <div>
+            {{ $message }}
+        </div>
+    @enderror
     <x-button type="submit" outlined color="white" class="inline-block">
         Sign Up
     </x-button>
