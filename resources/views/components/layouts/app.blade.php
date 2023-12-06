7717d11e3943701145633dc50a6cc71f5813af96
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-12-06 08:28:01 +0700

    refactor spacer handling

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,13 +4,14 @@
 <x-layouts.base {{ $attributes }} class="flex flex-col min-h-screen">
     @if ($forEmployer)
         <x-topbar.employer />
+        <div class="h-24 lg:h-28"></div>
     @else
         <x-topbar />
+        @auth
+            <div class="h-24 lg:h-28"></div>
+            <x-topbar.extension />
+        @endauth
     @endif
-    <div class="h-24 lg:h-28"></div>
-    @auth
-        <x-topbar.extension />
-    @endauth
     {{ $slot }}
     <div class="flex-grow"></div>
     <x-footer.extra />
