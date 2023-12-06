4c6fe0d62f1330dc42840a3ad0dcce8417b117e6
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2023-12-06 08:56:54 +0700

    optimize deal page

--- resources/views/components/deal-card.blade.php
+++ resources/views/components/deal-card.blade.php
@@ -1,27 +1,27 @@
 @props(['record'])
 
-<div class="space-y-2 mt-4 min-h-full flex flex-col">
+<div class="flex flex-col min-h-full mt-4 space-y-2">
 
-    <div class="flex-grow flex">
+    <div class="flex flex-col flex-grow">
         <div class="flex-1">
 
             @if ($record->brand->hasMedia('logo'))
-            <x-a class="inline-block h-16 overflow-hidden" :href="route('deals.show', ['id' => $record->slug])">
-                {{ $record->brand->getFirstMedia('logo')->img()->attributes(['class' => 'object-contain max-w-[6rem] w-full h-full']) }}
-            </x-a>
+                <x-a class="inline-block h-16 overflow-hidden" :href="route('deals.show', ['id' => $record->slug])">
+                    {{ $record->brand->getFirstMedia('logo')->img()->attributes(['class' => 'object-contain max-w-[6rem] w-full h-full']) }}
+                </x-a>
             @endif
         </div>
 
         <div class="flex-shrink-0">
             <x-a class="inline" :href="route('deals.show', ['id' => $record->slug])">
-                <h4 class="text-sm md:text-xl font-light">{{ $record->brand->name }}</h4>
+                <h4 class="text-4xl font-light">{{ $record->brand->name }}</h4>
             </x-a>
-            <p class="text-sm uppercase">{{ $record->name }}</p>
+            <p class="text-xl uppercase">{{ $record->name }}</p>
         </div>
     </div>
 
     <div class="h-20">
-        <p class=" line-clamp-3">{{ $record->excerpt }}</p>
+        <p class="line-clamp-3">{{ $record->excerpt }}</p>
     </div>
     <div class="flex-grow"></div>
 
