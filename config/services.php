cbaf0161eb540f02c115e06cb9f3b1fcdb5535ed
Michael Duffy <michaelrayduffy@gmail.com>
2023-12-10 15:31:43 +0000

    updating to use iam role for secret manager

--- config/services.php
+++ config/services.php
@@ -3,34 +3,55 @@
 require 'vendor/autoload.php';
 
 use Aws\SecretsManager\SecretsManagerClient;
+use Aws\Sts\StsClient;
 use Aws\Exception\AwsException;
 
 /**
- * Retrieves a secret from AWS Secrets Manager.
+ * Assumes an IAM role and retrieves a secret from AWS Secrets Manager.
+ *
  *
  * @param string $secretName The name of the secret.
  * @param string $region The AWS region where the secret is stored.
  * @return string|null The retrieved secret, or null in case of failure.
  */
-function getAwsSecret($secretName, $region = 'us-east-1') {
-    $client = new SecretsManagerClient([
+function getAwsSecret($secretName, $region = 'us-east-2') {
+    $stsClient = new StsClient([
         'version' => 'latest',
         'region' => $region
     ]);
 
     try {
-        $result = $client->getSecretValue([
-            'SecretId' => $secretName,
-            'VersionStage' => 'AWSCURRENT' // VersionStage defaults to AWSCURRENT if unspecified
+        // Assume the IAM role
+        $result = $stsClient->assumeRole([
+            'RoleArn' => "arn:aws:iam::891985934622:role/RoleToRetrieveSecretAtRuntime",
+            'RoleSessionName' => 'session-' . time() // You can customize the session name
         ]);
 
-        if (isset($result['SecretString'])) {
-            return $result['SecretString'];
+        $credentials = $result['Credentials'];
+
+        // Create SecretsManagerClient with the assumed role credentials
+        $secretsManagerClient = new SecretsManagerClient([
+            'version' => 'latest',
+            'region' => $region,
+            'credentials' => [
+                'key' => $credentials['AccessKeyId'],
+                'secret' => $credentials['SecretAccessKey'],
+                'token' => $credentials['SessionToken']
+            ]
+        ]);
+
+        // Retrieve the secret
+        $response = $secretsManagerClient->getSecretValue([
+            'SecretId' => $secretName,
+            'VersionStage' => 'AWSCURRENT'
+        ]);
+
+        if (isset($response['SecretString'])) {
+            return $response['SecretString'];
         } else {
-            return base64_decode($result['SecretBinary']);
+            return base64_decode($response['SecretBinary']);
         }
     } catch (AwsException $e) {
-        // Log the error or handle it as needed
         error_log($e->getMessage());
         return null;
     }
