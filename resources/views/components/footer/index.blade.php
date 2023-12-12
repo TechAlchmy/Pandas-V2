ddb264614ea65c063178425178f0cf14c066fded
bitcooker <robertmercado0407@gmail.com>
2023-12-12 06:41:15 -0500

    guest_homepage

--- resources/views/components/footer/index.blade.php
+++ resources/views/components/footer/index.blade.php
@@ -2,13 +2,13 @@
     <div class="px-[min(6.99vw,50px)] lg:pl-12 h-[75px] lg:h-[100px]">
         <div class="mx-auto max-w-[1920px]">
             <div class="w-full flex flex-col justify-between items-center">
-                <div class="h-48"></div>
+                <!-- <div class="h-48"></div> -->
                 <div x-show="shown" x-transition:enter.duration.1000ms.opactiy class="w-full">
                     <x-hr />
                     <div class="w-full my-5">
-                        <div class="flex justify-start mt-10">
-                            <p class="text-[20px] mr-10 animate-fadeIn">Panda People® {{ date('Y') }} © All Rights Reserved</p>
-                            <a href="#" class="text-[20px] hover:text-blue-800 ml-10">Privacy Policy</a>
+                        <div class="flex justify-end gap-x-8">
+                            <p class="text-[1rem] animate-fadeIn">Panda People® {{ date('Y') }} © All Rights Reserved</p>
+                            <a href="#" class="text-[1rem] hover:text-blue-800">Privacy Policy</a>
                         </div>
                     </div>
                 </div>
